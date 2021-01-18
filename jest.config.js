246723ea63d6b25e23f0bb01228c688068246af8
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-18 14:50:25 +0700

    Ignore 'build' directory in tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules/', '/build/'],
     globals: {
         'ts-jest': {
             tsconfig: 'tsconfig.jest.json',
