92161091c6f5bedfdf4712458b54a069db997fe8
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-18 21:34:38 +0700

    Add custom font to the example

--- packages/slate-examples/src/pages/index.tsx
+++ packages/slate-examples/src/pages/index.tsx
@@ -2,9 +2,10 @@ import { render } from '@prezly/slate-renderer';
 import React from 'react';
 
 import story from './story.json';
+import styles from './index.module.scss';
 
 const Index = () => {
-    return <article style={{ margin: '0 auto', width: 640 }}>{render(story.children)}</article>;
+    return <article className={styles.article}>{render(story.children)}</article>;
 };
 
 export default Index;
