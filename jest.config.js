80fbb13d2980dd460ac34011411b2ad646712894
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-18 16:07:55 +0700

    Exclude examples from tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/build/'],
+    testPathIgnorePatterns: ['/node_modules/', '/build/', '/examples/'],
     globals: {
         'ts-jest': {
             tsconfig: 'tsconfig.jest.json',
