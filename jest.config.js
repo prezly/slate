f092dbd46b63ccf266a18c889143b1a1ae4722df
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-19 14:06:53 +0700

    Fix running tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/build/', '/examples/'],
     globals: {
         'ts-jest': {
             tsconfig: 'tsconfig.jest.json',
         },
     },
+    moduleNameMapper: {
+        '\\.scss$': '<rootDir>/styleMock',
+    },
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules/', '/build/', '/examples/'],
 };
