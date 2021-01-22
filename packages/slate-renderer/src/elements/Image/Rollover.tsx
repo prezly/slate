e6db1472a099f0a675ef47525a7ae45d75bb0aac
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-22 21:56:56 +0700

    Improve styling

--- packages/slate-renderer/src/elements/Image/Rollover.tsx
+++ packages/slate-renderer/src/elements/Image/Rollover.tsx
@@ -1,21 +1,26 @@
 import classNames from 'classnames';
-import React, { FunctionComponent, HTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, FunctionComponent } from 'react';
 
 import { ArrowsAngleExpand } from '../../icons';
 
 import './Rollover.scss';
 
-interface Props extends HTMLAttributes<HTMLDivElement> {
-    disabled?: boolean;
-}
-
-const Rollover: FunctionComponent<Props> = ({ children, className, disabled, ...props }) => {
+const Rollover: FunctionComponent<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+    children,
+    className,
+    disabled,
+    ...props
+}) => {
     if (disabled) {
         return <>{children}</>;
     }
 
     return (
-        <div className={classNames('prezly-slate-image-rollover', className)} {...props}>
+        <button
+            className={classNames('prezly-slate-image-rollover', className)}
+            type="button"
+            {...props}
+        >
             {children}
 
             <div className="prezly-slate-image-rollover__caption">
@@ -23,7 +28,7 @@ const Rollover: FunctionComponent<Props> = ({ children, className, disabled, ...
                     <ArrowsAngleExpand className="prezly-slate-image-rollover__caption-icon" />
                 </span>
             </div>
-        </div>
+        </button>
     );
 };
 
