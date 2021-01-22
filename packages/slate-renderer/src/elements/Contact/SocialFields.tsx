0c1999d85b9d29cf54cd1826a4fad99b8d6b0feb
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-22 15:07:44 +0700

    Improve Attachment & Contact styling on smaller screens

--- packages/slate-renderer/src/elements/Contact/SocialFields.tsx
+++ packages/slate-renderer/src/elements/Contact/SocialFields.tsx
@@ -10,44 +10,44 @@ interface Props {
 const SocialFields: FunctionComponent<Props> = ({ contact }) => (
     <ul className="prezly-slate-contact__social-fields">
         {contact.email && (
-            <li className="prezly-slate-contact__social-field">
+            <li className="prezly-slate-contact__social-field" title={contact.email}>
                 <img className="prezly-slate-contact__social-field-icon" src={envelope} />
-                {contact.email}
+                <span className="prezly-slate-contact__social-field-value">{contact.email}</span>
             </li>
         )}
 
         {contact.phone && (
-            <li className="prezly-slate-contact__social-field">
+            <li className="prezly-slate-contact__social-field" title={contact.phone}>
                 <img className="prezly-slate-contact__social-field-icon" src={telephone} />
-                {contact.phone}
+                <span className="prezly-slate-contact__social-field-value">{contact.phone}</span>
             </li>
         )}
 
         {contact.mobile && (
-            <li className="prezly-slate-contact__social-field">
+            <li className="prezly-slate-contact__social-field" title={contact.mobile}>
                 <img className="prezly-slate-contact__social-field-icon" src={phone} />
-                {contact.mobile}
+                <span className="prezly-slate-contact__social-field-value">{contact.mobile}</span>
             </li>
         )}
 
         {contact.twitter && (
-            <li className="prezly-slate-contact__social-field">
+            <li className="prezly-slate-contact__social-field" title={contact.twitter}>
                 <img className="prezly-slate-contact__social-field-icon" src={twitter} />
-                {contact.twitter}
+                <span className="prezly-slate-contact__social-field-value">{contact.twitter}</span>
             </li>
         )}
 
         {contact.facebook && (
-            <li className="prezly-slate-contact__social-field">
+            <li className="prezly-slate-contact__social-field" title={contact.facebook}>
                 <img className="prezly-slate-contact__social-field-icon" src={facebook} />
-                {contact.facebook}
+                <span className="prezly-slate-contact__social-field-value">{contact.facebook}</span>
             </li>
         )}
 
         {contact.website && (
-            <li className="prezly-slate-contact__social-field">
+            <li className="prezly-slate-contact__social-field" title={contact.website}>
                 <img className="prezly-slate-contact__social-field-icon" src={window} />
-                {contact.website}
+                <span className="prezly-slate-contact__social-field-value">{contact.website}</span>
             </li>
         )}
     </ul>
