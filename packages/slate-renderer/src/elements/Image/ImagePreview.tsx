eee66f487f9c587c7f6fc1da2846160db1eea28a
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-26 19:35:39 +0700

    Use downloadUrl for sharing on Pinterest

--- packages/slate-renderer/src/elements/Image/ImagePreview.tsx
+++ packages/slate-renderer/src/elements/Image/ImagePreview.tsx
@@ -52,7 +52,7 @@ const ImagePreview: FunctionComponent<Props> = ({ children, className, file, isO
 
                         <PinterestButton
                             className="prezly-slate-image-preview__pinterest"
-                            image={image.cdnUrl}
+                            image={image.downloadUrl}
                         />
                     </div>
                 </div>
