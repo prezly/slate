d52721b0b33c83b5284d1347990e8a0975d0ef66
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-26 16:39:40 +0700

    Add Pinterest icon

--- packages/slate-renderer/src/icons/index.ts
+++ packages/slate-renderer/src/icons/index.ts
@@ -4,6 +4,7 @@ export { default as Download } from './Download.svg';
 export { default as Envelope } from './Envelope.svg';
 export { default as Facebook } from './Facebook.svg';
 export { default as PersonFill } from './PersonFill.svg';
+export { default as Pinterest } from './Pinterest.svg';
 export { default as Phone } from './Phone.svg';
 export { default as Telephone } from './Telephone.svg';
 export { default as Twitter } from './Twitter.svg';
