41b2645b20ebd0e946b56b8c31d2cf6c3881a2c8
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-02-12 14:40:52 +0700

    Make sure to export everything from @prezly/slate-lists

--- packages/slate-lists/src/index.ts
+++ packages/slate-lists/src/index.ts
@@ -1,5 +1,5 @@
+export * from './lib';
 export { default as Lists } from './Lists';
-export { ListsOptions } from './types';
+export * from './types';
 export { default as withLists } from './withLists';
 export { default as withListsReact } from './withListsReact';
-export { isList, isListItem, isListItemText } from './lib';
