d917f81dc665d8b28feee30b1fc9786045d624b7
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-03-16 19:37:24 +0700

    Inline postcss config to webpack config

--- packages/slate-editor/webpack.config.js
+++ packages/slate-editor/webpack.config.js
@@ -1,8 +1,8 @@
+const autoprefixer = require('autoprefixer');
 const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
-const postCssConfig = require('./postcss.config');
 
 const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 const tsConfig = JSON.parse(fs.readFileSync('./tsconfig.json', 'utf-8'));
@@ -27,7 +27,12 @@ module.exports = {
                 use: [
                     MiniCssExtractPlugin.loader,
                     { loader: 'css-loader' },
-                    { loader: 'postcss-loader', options: postCssConfig },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [autoprefixer({ grid: true })],
+                        },
+                    },
                     { loader: 'sass-loader' },
                 ],
             },
