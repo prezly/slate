6c963a36bbf466df86a326d9d0d19a33a3eaa04b
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-03-17 16:16:29 +0700

    Add default export

--- packages/slate-editor/src/index.ts
+++ packages/slate-editor/src/index.ts
@@ -1,5 +1,6 @@
 export * from './components';
 export * from './icons';
 export * from './lib';
+export { default } from './modules/editor-v4';
 export * from './modules/editor-v4';
 export * from './types';
