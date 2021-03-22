29a407b4102767f8386350fd0ad4de8f11d25395
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-03-22 14:38:00 +0700

    Don't remove viewbox when transforming SVG icons with SVGR

--- packages/slate-editor/webpack.config.js
+++ packages/slate-editor/webpack.config.js
@@ -37,7 +37,17 @@ module.exports = {
             },
             {
                 test: /\.svg$/,
-                use: '@svgr/webpack',
+                use: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            svgoConfig: {
+                                removeViewBox: false,
+                            },
+                            typescript: true,
+                        },
+                    },
+                ],
             },
         ],
     },
