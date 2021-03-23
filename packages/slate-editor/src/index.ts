ffa79da10e857667ad7df6495361864a65c106c4
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-03-23 15:36:52 +0700

    Fix exports

--- packages/slate-editor/src/index.ts
+++ packages/slate-editor/src/index.ts
@@ -1,14 +1,14 @@
-export * from 'components';
-export * from 'lib';
-export { default } from 'modules/editor-v4';
-export * from 'modules/editor-v4';
-export { COVERAGE_TYPE, SearchProps as CoverageSearchProps } from 'modules/editor-v4-coverage';
-export { createEmbed } from 'modules/editor-v4-embed';
+export * from './components';
+export * from './lib';
+export { default } from './modules/editor-v4';
+export * from './modules/editor-v4';
+export { COVERAGE_TYPE, SearchProps as CoverageSearchProps } from './modules/editor-v4-coverage';
+export { createEmbed } from './modules/editor-v4-embed';
 export {
     JobDescription,
     SearchProps as PressContactsSearchProps,
-} from 'modules/editor-v4-press-contacts';
-export { ElementType as RichElementType } from 'modules/editor-v4-rich-formatting';
-export { ResultPromise, UploadcareOptions, withUploadcare } from 'modules/editor-v4-uploadcare';
-export { User } from 'modules/editor-v4-user-mentions';
-export * from 'types';
+} from './modules/editor-v4-press-contacts';
+export { ElementType as RichElementType } from './modules/editor-v4-rich-formatting';
+export { ResultPromise, UploadcareOptions, withUploadcare } from './modules/editor-v4-uploadcare';
+export { User } from './modules/editor-v4-user-mentions';
+export * from './types';
