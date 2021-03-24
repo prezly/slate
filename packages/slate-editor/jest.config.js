d3e9fb0c36865694aa6fced8b3c86a8392658953
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-03-24 12:57:35 +0700

    Remove aliases from jest config

--- packages/slate-editor/jest.config.js
+++ packages/slate-editor/jest.config.js
@@ -5,21 +5,11 @@ module.exports = {
             tsconfig: '<rootDir>/tsconfig.jest.json',
         },
     },
-    "moduleNameMapper": {
+    moduleNameMapper: {
         '\\.(scss|svg)$': '<rootDir>/../../universalMock',
-        "^components$": "<rootDir>/src/components",
-        "^lib$": "<rootDir>/src/lib",
-        "^lib/(.*)$": "<rootDir>/src/lib/$1",
-        "^icons$": "<rootDir>/src/icons",
-        "^modules/(.*)$": "<rootDir>/src/modules/$1",
-        "^types$": "<rootDir>/src/types"
     },
     preset: 'ts-jest',
     setupFiles: ['<rootDir>/../../setupTests.js'],
     testEnvironment: 'jsdom',
-    testPathIgnorePatterns: [
-        '/node_modules/',
-        '/build/',
-        '/src/modules/(?!editor-v4-image/withImages.test.tsx)'
-    ]
+    testPathIgnorePatterns: ['/node_modules/', '/build/'],
 };
