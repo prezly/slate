43b774d6a29f118d989454099b65797aaa23386d
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-09-07 14:48:48 +0200

    Fix react duplication issue

---
+++ packages/playground/config-overrides.js
@@ -0,0 +1,11 @@
+const {
+  override,
+  addWebpackAlias,
+} = require("customize-cra");
+const path = require("path");
+
+module.exports = override( 
+    addWebpackAlias({
+        react: path.resolve('./node_modules/react')
+    })
+);
