60761532a294f6ddd3b1e3e332411789f561eb33
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-09-07 15:20:52 +0200

    Fix initial editor value

--- packages/playground/src/Editor/Editor.tsx
+++ packages/playground/src/Editor/Editor.tsx
@@ -1,11 +1,14 @@
+import { createEmptyValue } from '@prezly/slate-commons';
+import SlateEditor from '@prezly/slate-editor';
 import { FunctionComponent, useState } from 'react';
 import { Node } from 'slate';
-import SlateEditor from '@prezly/slate-editor';
+
+const INITIAL_VALUE = createEmptyValue();
 
 const Editor: FunctionComponent = () => {
-    const [state, setState] = useState<Node[]>([]);
+    const [value, setValue] = useState<Node[]>(INITIAL_VALUE);
 
-    return <SlateEditor availableWidth={900} onChange={setState} value={state} />;
+    return <SlateEditor availableWidth={900} onChange={setValue} value={value} />;
 };
 
 export default Editor;
