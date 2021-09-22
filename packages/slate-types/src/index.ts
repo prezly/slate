155472f0fb6bc6f474251a72535081732d9419c9
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-09-22 11:46:08 +0200

    Update custom slate types

--- packages/slate-types/src/index.ts
+++ packages/slate-types/src/index.ts
@@ -3,16 +3,14 @@ export * from './lib';
 export * from './nodes';
 export * from './sdk';
 
-// import { InlineNode } from '@prezly/slate-types';
 import { BaseEditor } from 'slate';
 import { ReactEditor } from 'slate-react';
 
-// import { BlockNode } from './nodes';
+import { BlockNode } from './nodes';
 
 declare module 'slate' {
     interface CustomTypes {
         Editor: BaseEditor & ReactEditor;
-        // Element: BlockNode;
-        // Text: InlineNode;
+        Element: BlockNode;
     }
 }
