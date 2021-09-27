d46f13634d8bd352b1a919b1c7af7e90687636ec
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-09-27 19:49:55 +0700

    Remove invalid export

--- packages/slate-editor/src/modules/editor-v4-galleries/lib/index.ts
+++ packages/slate-editor/src/modules/editor-v4-galleries/lib/index.ts
@@ -1,6 +1,5 @@
 export { default as createGallery } from './createGallery';
 export { default as getCurrentGalleryNodeEntry } from './getCurrentGalleryNodeEntry';
-export { default as isGalleryElement } from './isGalleryElement';
 export { default as normalizeInvalidGallery } from './normalizeInvalidGallery';
 export { default as normalizeRedundantGalleryAttributes } from './normalizeRedundantGalleryAttributes';
 export { default as parseSerializedElement } from './parseSerializedElement';
