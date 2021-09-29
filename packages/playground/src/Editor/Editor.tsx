2e0dd483293eff2489a2244d95173bd80f6e6324
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-09-29 19:31:22 +0700

    Fix typing in Editor

--- packages/playground/src/Editor/Editor.tsx
+++ packages/playground/src/Editor/Editor.tsx
@@ -1,10 +1,9 @@
 import { createEmptyValue } from '@prezly/slate-commons';
 import SlateEditor from '@prezly/slate-editor';
 import { FunctionComponent, useState } from 'react';
-import { Node } from 'slate';
 
 const Editor: FunctionComponent = () => {
-    const [value, setValue] = useState<Node[]>(createEmptyValue());
+    const [value, setValue] = useState(createEmptyValue());
 
     return <SlateEditor availableWidth={900} onChange={setValue} value={value} />;
 };
