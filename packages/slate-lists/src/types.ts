668f03e4270f320208327acbadbb39b7bfd1710b
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-05 15:32:43 +0200

    Move type declarations

--- packages/slate-lists/src/types.ts
+++ packages/slate-lists/src/types.ts
@@ -1,5 +1,3 @@
-import { ListItemNode, ListItemTextNode, TextNode } from '@prezly/slate-types';
-
 export interface ListsOptions {
     /**
      * Type of the node that "listItemTextType" will become when it is unwrapped or normalized.
@@ -25,11 +23,4 @@ export interface ListsOptions {
      * Types of nodes that can be converted into a node representing list item text.
      */
     wrappableTypes: string[];
-}
-
-declare module 'slate' {
-    interface AdditionalCustomTypes {
-        Element: ListItemNode | ListItemTextNode;
-        Text: TextNode;
-    }
 }
