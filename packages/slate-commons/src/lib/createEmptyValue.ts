6de22ea78a7f536aa1a6aa277ebb07fcd914c411
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-06 14:21:11 +0200

    Refactor createEmptyValue

--- packages/slate-commons/src/lib/createEmptyValue.ts
+++ packages/slate-commons/src/lib/createEmptyValue.ts
@@ -1,6 +1,7 @@
-import { ElementNode, PARAGRAPH_NODE_TYPE } from '@prezly/slate-types';
+import { PARAGRAPH_NODE_TYPE } from '@prezly/slate-types';
+import { Descendant } from 'slate';
 
-const createEmptyValue = (): ElementNode[] => [
+const createEmptyValue = (): Descendant[] => [
     {
         children: [{ text: '' }],
         type: PARAGRAPH_NODE_TYPE,
