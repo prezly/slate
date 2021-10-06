246db9e52f29dc37eb5f84d6d97021197448ef75
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-06 12:35:22 +0200

    Refactor createPressContact

--- packages/slate-editor/src/modules/editor-v4-press-contacts/lib/createPressContact.ts
+++ packages/slate-editor/src/modules/editor-v4-press-contacts/lib/createPressContact.ts
@@ -1,6 +1,6 @@
 import { ContactNode, CONTACT_NODE_TYPE, PressContact } from '@prezly/slate-types';
 
-const createPressContact = (contact: PressContact & { uuid: string }): ContactNode => ({
+const createPressContact = (contact: PressContact): ContactNode => ({
     children: [{ text: '' }],
     contact,
     type: CONTACT_NODE_TYPE,
