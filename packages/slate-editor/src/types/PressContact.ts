a75d9696762fa0adfe086035e5a217c4f76ee2c6
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-06 12:35:14 +0200

    Update PressContact interface

--- packages/slate-editor/src/types/PressContact.ts
+++ packages/slate-editor/src/types/PressContact.ts
@@ -1,13 +1,17 @@
 export default interface PressContact {
     avatar_url: string | null;
     company: string | null;
+    // TODO: cultures
     description: string | null;
     email: string | null;
     facebook: string | null;
     id: number;
+    // TODO: is_visible_in_room
     mobile: string | null;
     name: string;
+    // TODO: order
     phone: string | null;
+    // TODO: stories
     twitter: string | null;
     uuid: string;
     website: string | null;
