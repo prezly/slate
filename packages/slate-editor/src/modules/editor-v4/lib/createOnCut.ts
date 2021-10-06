788a8246cd85870506f15c3b69933f838205eb5f
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-06 14:21:44 +0200

    Refactor createOnCut

--- packages/slate-editor/src/modules/editor-v4/lib/createOnCut.ts
+++ packages/slate-editor/src/modules/editor-v4/lib/createOnCut.ts
@@ -1,9 +1,7 @@
 import { Editor, Range, Transforms } from 'slate';
 import { ReactEditor } from 'slate-react';
 
-const createOnCut = (editor: ReactEditor) => (
-    event: React.ClipboardEvent<HTMLDivElement>,
-): void => {
+const createOnCut = (editor: Editor) => (event: React.ClipboardEvent<HTMLDivElement>): void => {
     event.preventDefault();
 
     ReactEditor.setFragmentData(editor, event.clipboardData);
