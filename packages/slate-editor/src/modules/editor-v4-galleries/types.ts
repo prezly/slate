1312de36e1567fa5bfc2044c0372637784ac0de7
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-06 14:37:16 +0200

    Remove GalleryNode enums from slate-editor

--- packages/slate-editor/src/modules/editor-v4-galleries/types.ts
+++ packages/slate-editor/src/modules/editor-v4-galleries/types.ts
@@ -2,26 +2,6 @@ import { NewsroomRef } from '@prezly/sdk';
 import { RefObject } from 'react';
 import { Editor } from 'slate';
 
-export enum GalleryLayout {
-    CONTAINED = 'contained',
-    EXPANDED = 'expanded',
-    FULL_WIDTH = 'full-width',
-}
-
-export enum GalleryImageSize {
-    L = 'L',
-    M = 'M',
-    S = 'S',
-    XL = 'XL',
-    XS = 'XS',
-}
-
-export enum GalleryPadding {
-    L = 'L',
-    M = 'M',
-    S = 'S',
-}
-
 export interface GalleriesExtensionParameters {
     mediaGalleryTab?: {
         newsroom: NewsroomRef;
