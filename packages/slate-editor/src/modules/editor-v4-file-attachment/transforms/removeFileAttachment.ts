71442032210a1bdc6668d72611a35f0119df97cd
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-06 13:10:35 +0200

    Refactor removeFileAttachment

--- packages/slate-editor/src/modules/editor-v4-file-attachment/transforms/removeFileAttachment.ts
+++ packages/slate-editor/src/modules/editor-v4-file-attachment/transforms/removeFileAttachment.ts
@@ -1,12 +1,10 @@
 import { EditorCommands } from '@prezly/slate-commons';
+import { AttachmentNode, isAttachmentNode } from '@prezly/slate-types';
 import { Editor } from 'slate';
 
-import { isFileAttachmentElement } from '../lib';
-import { FileAttachmentElementType } from '../types';
-
-const removeFileAttachment = (editor: Editor): FileAttachmentElementType | null =>
-    EditorCommands.removeNode<FileAttachmentElementType>(editor, {
-        match: isFileAttachmentElement,
+const removeFileAttachment = (editor: Editor): AttachmentNode | null =>
+    EditorCommands.removeNode<AttachmentNode>(editor, {
+        match: isAttachmentNode,
     });
 
 export default removeFileAttachment;
