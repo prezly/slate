10cf9a06dfce397e7054481dabc9becb7bc74bff
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-07 13:18:13 +0200

    Fix playground Editor typing

--- packages/playground/src/Editor/Editor.tsx
+++ packages/playground/src/Editor/Editor.tsx
@@ -1,9 +1,10 @@
 import { createEmptyValue } from '@prezly/slate-commons';
 import SlateEditor from '@prezly/slate-editor';
+import { BlockNode } from '@prezly/slate-types';
 import { FunctionComponent, useState } from 'react';
 
 const Editor: FunctionComponent = () => {
-    const [value, setValue] = useState(createEmptyValue());
+    const [value, setValue] = useState<BlockNode[]>(createEmptyValue());
 
     return <SlateEditor availableWidth={900} onChange={setValue} value={value} />;
 };
