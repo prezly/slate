ad3ec811c6d32def4957f1f444b11a7f1ca4ef0f
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-07 12:54:23 +0200

    Fix handleRemoveAttachment type

--- packages/slate-editor/src/modules/editor-v4/lib/file-attachment/handleRemoveAttachment.ts
+++ packages/slate-editor/src/modules/editor-v4/lib/file-attachment/handleRemoveAttachment.ts
@@ -1,9 +1,9 @@
+import { AttachmentNode } from '@prezly/slate-types';
 import { Editor } from 'slate';
 
 import { EventsEditor } from '../../../../modules/editor-v4-events';
-import { FileAttachmentElementType } from '../../../../modules/editor-v4-file-attachment';
 
-const handleRemoveAttachment = (editor: Editor, element: FileAttachmentElementType): void => {
+const handleRemoveAttachment = (editor: Editor, element: AttachmentNode): void => {
     EventsEditor.dispatchEvent(editor, 'attachment-removed', { uuid: element.file.uuid });
 };
 
