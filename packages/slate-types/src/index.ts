060042d324e97c7dee51c2fe2bd82ba17761363b
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-10-14 19:33:38 +0700

    Change custom typing of Editor to include HistoryEditor, add slate-history peer-dependency to @prezly/slate-types

--- packages/slate-types/src/index.ts
+++ packages/slate-types/src/index.ts
@@ -4,6 +4,7 @@ export * from './nodes';
 export * from './sdk';
 
 import { BaseEditor } from 'slate';
+import { HistoryEditor } from 'slate-history';
 import { ReactEditor } from 'slate-react';
 
 import { BlockNode, InlineNode, ListItemNode, ListItemTextNode, TextNode } from './nodes';
@@ -13,7 +14,7 @@ export declare interface AdditionalCustomTypes {
 }
 
 declare module 'slate' {
-    type DefaultEditor = BaseEditor & ReactEditor;
+    type DefaultEditor = BaseEditor & ReactEditor & HistoryEditor;
 
     type DefaultNode = BlockNode | InlineNode | ListItemNode | ListItemTextNode;
 
