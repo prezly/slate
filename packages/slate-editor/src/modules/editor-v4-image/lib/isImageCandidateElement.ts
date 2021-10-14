5dc45a21ef94f5a29922792206e7b39de6c5ac95
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-10-14 18:18:47 +0700

    Make type guards more generic to fix typing

--- packages/slate-editor/src/modules/editor-v4-image/lib/isImageCandidateElement.ts
+++ packages/slate-editor/src/modules/editor-v4-image/lib/isImageCandidateElement.ts
@@ -1,10 +1,9 @@
 import { isElementNode } from '@prezly/slate-types';
-import { Node } from 'slate';
 
 import { IMAGE_CANDIDATE_TYPE } from '../constants';
 import { ImageCandidateElementType } from '../types';
 
-const isImageCandidateElement = (node: Node): node is ImageCandidateElementType =>
+const isImageCandidateElement = (node: unknown): node is ImageCandidateElementType =>
     isElementNode(node) && node.type === IMAGE_CANDIDATE_TYPE;
 
 export default isImageCandidateElement;
--- packages/slate-editor/src/modules/editor-v4-rich-formatting/lib/isLinkCandidateElement.ts
+++ packages/slate-editor/src/modules/editor-v4-rich-formatting/lib/isLinkCandidateElement.ts
@@ -1,9 +1,8 @@
 import { isElementNode } from '@prezly/slate-types';
-import { Node } from 'slate';
 
 import { ElementType, LinkCandidateElementType } from '../types';
 
-const isLinkCandidateElement = (node: Node): node is LinkCandidateElementType =>
+const isLinkCandidateElement = (node: unknown): node is LinkCandidateElementType =>
     isElementNode(node) && node.type === ElementType.LINK_CANDIDATE;
 
 export default isLinkCandidateElement;
