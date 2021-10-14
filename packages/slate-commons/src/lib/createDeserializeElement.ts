0629eea368662540052f955f8688c85c0604ea10
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-10-14 17:53:24 +0700

    Fix createDeserializeElement typing

--- packages/slate-commons/src/lib/createDeserializeElement.ts
+++ packages/slate-commons/src/lib/createDeserializeElement.ts
@@ -1,8 +1,8 @@
-import { ElementNode } from '@prezly/slate-types';
+import { Element } from 'slate';
 
-type Parse<E extends ElementNode> = (serialized: string) => E | undefined;
+type Parse<E extends Element> = (serialized: string) => E | undefined;
 
-const createDeserializeElement = <E extends ElementNode>(parse: Parse<E>) => {
+const createDeserializeElement = <E extends Element>(parse: Parse<E>) => {
     return (element: HTMLElement): E | undefined => {
         const serialized = element.getAttribute('data-slate-value');
 
