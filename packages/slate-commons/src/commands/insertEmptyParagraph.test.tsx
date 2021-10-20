265a72e455da6227bb8e8fd96837e9e696dc3c85
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-10-20 14:47:21 +0300

    Extend insertEmptyParagraph test to cover edge-cases
    To be sure that createHandleEditImage() still works as expected,
    as it relies on inserting new empty paragraphs.

--- packages/slate-commons/src/commands/insertEmptyParagraph.test.tsx
+++ packages/slate-commons/src/commands/insertEmptyParagraph.test.tsx
@@ -7,12 +7,11 @@ import jsx from '../jsx';
 import insertEmptyParagraph from './insertEmptyParagraph';
 
 describe('insertEmptyParagraph', () => {
-    it('Inserts an empty paragraph after the cursor', () => {
+    it('should insert an empty paragraph after the cursor', () => {
         const editor = ((
             <editor>
                 <h-p>
-                    <h-text>lorem ipsum</h-text>
-                    <cursor />
+                    <h-text>lorem ipsum<cursor /></h-text>
                 </h-p>
             </editor>
         ) as unknown) as Editor;
@@ -23,8 +22,9 @@ describe('insertEmptyParagraph', () => {
                     <h-text>lorem ipsum</h-text>
                 </h-p>
                 <h-p>
-                    <h-text />
-                    <cursor />
+                    <h-text>
+                        <cursor />
+                    </h-text>
                 </h-p>
             </editor>
         ) as unknown) as Editor;
@@ -35,13 +35,11 @@ describe('insertEmptyParagraph', () => {
         expect(editor.selection).toEqual(expected.selection);
     });
 
-    it('Inserts an empty paragraph at the cursor, splitting the text in the block', () => {
+    it('should insert an empty paragraph at the cursor, splitting the text in the block', () => {
         const editor = ((
             <editor>
                 <h-p>
-                    <h-text>lorem</h-text>
-                    <cursor />
-                    <h-text>ipsum</h-text>
+                    <h-text>lorem<cursor />ipsum</h-text>
                 </h-p>
             </editor>
         ) as unknown) as Editor;
@@ -52,8 +50,9 @@ describe('insertEmptyParagraph', () => {
                     <h-text>lorem</h-text>
                 </h-p>
                 <h-p>
-                    <h-text />
-                    <cursor />
+                    <h-text>
+                        <cursor />
+                    </h-text>
                 </h-p>
                 <h-p>
                     <h-text>ipsum</h-text>
@@ -67,15 +66,90 @@ describe('insertEmptyParagraph', () => {
         expect(editor.selection).toEqual(expected.selection);
     });
 
-    it('Inserts an empty paragraph at a given location', () => {
+    /**
+     * createHandleEditImage() relies on this behavior.
+     * @see createHandleEditImage()
+     */
+    it('should insert an empty paragraph after the current one, if the cursor is at the end', () => {
+        const editor = ((
+            <editor>
+                <h-p>
+                    <h-text>lorem ipsum<cursor /></h-text>
+                </h-p>
+                <h-p>
+                    <h-text>dolor</h-text>
+                </h-p>
+            </editor>
+        ) as unknown) as Editor;
+
+        const expected = ((
+            <editor>
+                <h-p>
+                    <h-text>lorem ipsum</h-text>
+                </h-p>
+                <h-p>
+                    <h-text>
+                        <cursor />
+                    </h-text>
+                </h-p>
+                <h-p>
+                    <h-text>dolor</h-text>
+                </h-p>
+            </editor>
+        ) as unknown) as Editor;
+
+        insertEmptyParagraph(editor);
+
+        expect(editor.children).toEqual(expected.children);
+        expect(editor.selection).toEqual(expected.selection);
+    });
+
+    /**
+     * createHandleEditImage() relies on this behavior.
+     * @see createHandleEditImage()
+     */
+    it('should insert an empty paragraph before the current one, if the cursor is at the beginning', () => {
+        const editor = ((
+            <editor>
+                <h-p>
+                    <h-text>lorem ipsum</h-text>
+                </h-p>
+                <h-p>
+                    <h-text><cursor />dolor</h-text>
+                </h-p>
+            </editor>
+        ) as unknown) as Editor;
+
+        const expected = ((
+            <editor>
+                <h-p>
+                    <h-text>lorem ipsum</h-text>
+                </h-p>
+                <h-p>
+                    <h-text>
+                        <cursor />
+                    </h-text>
+                </h-p>
+                <h-p>
+                    <h-text>dolor</h-text>
+                </h-p>
+            </editor>
+        ) as unknown) as Editor;
+
+        insertEmptyParagraph(editor);
+
+        expect(editor.children).toEqual(expected.children);
+        expect(editor.selection).toEqual(expected.selection);
+    });
+
+    it('should insert an empty paragraph at a given location', () => {
         const editor = ((
             <editor>
                 <h-p>
                     <h-text>first block</h-text>
                 </h-p>
                 <h-p>
-                    <h-text>second block</h-text>
-                    <cursor />
+                    <h-text>second block<cursor /></h-text>
                 </h-p>
             </editor>
         ) as unknown) as Editor;
@@ -89,8 +163,7 @@ describe('insertEmptyParagraph', () => {
                     <h-text />
                 </h-p>
                 <h-p>
-                    <h-text>second block</h-text>
-                    <cursor />
+                    <h-text>second block<cursor /></h-text>
                 </h-p>
             </editor>
         ) as unknown) as Editor;
