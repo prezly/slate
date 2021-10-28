6d0a04ac7c874ad62ed59b7bdefc6d3a645d286c
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-10-28 16:35:04 +0300

    Drop `AdditionalCustomTypes`
    As there's not much need in it now, after we've inverted the types.

--- packages/slate-lists/src/@types/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { ListItemNode, ListItemTextNode } from '@prezly/slate-types';
-
-declare module '@prezly/slate-types' {
-    interface AdditionalCustomTypes {
-        Element: ListItemNode | ListItemTextNode;
-    }
-}
--- packages/slate-types/src/index.ts
+++ packages/slate-types/src/index.ts
@@ -3,32 +3,19 @@ export * from './lib';
 export * from './nodes';
 export * from './sdk';
 
-import { BaseEditor, Descendant } from 'slate';
+import { BaseEditor, BaseElement } from 'slate';
 import { ReactEditor } from 'slate-react';
 
 import { TextNode } from './nodes';
 
-export declare interface AdditionalCustomTypes {
-    [key: string]: unknown;
-}
-
-interface ElementNode {
+export interface ElementNode extends BaseElement {
     type: string;
-    children: Descendant[];
 }
 
 declare module 'slate' {
-    type DefaultEditor = BaseEditor & ReactEditor;
-
     interface CustomTypes {
-        Editor: unknown extends AdditionalCustomTypes['Editor']
-            ? DefaultEditor
-            : DefaultEditor & AdditionalCustomTypes['Editor'];
-        Element: unknown extends AdditionalCustomTypes['Element']
-            ? ElementNode
-            : ElementNode & AdditionalCustomTypes['Element'];
-        Text: unknown extends AdditionalCustomTypes['Text']
-            ? TextNode
-            : TextNode & AdditionalCustomTypes['Text'];
+        Editor: BaseEditor & ReactEditor;
+        Element: ElementNode;
+        Text: TextNode;
     }
 }
