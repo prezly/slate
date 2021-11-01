cb8d99a7c0d06c47cc75143e129f6719e7197bac
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-02 00:13:10 +0200

    Fix `normalizeRedundantImageAttributes()`

--- packages/slate-editor/src/modules/editor-v4-image/lib/normalizeRedundantImageAttributes.ts
+++ packages/slate-editor/src/modules/editor-v4-image/lib/normalizeRedundantImageAttributes.ts
@@ -1,21 +1,22 @@
 import { EditorCommands } from '@prezly/slate-commons';
-import { isImageNode } from '@prezly/slate-types';
+import { ImageNode, isImageNode } from '@prezly/slate-types';
 import { Editor, NodeEntry } from 'slate';
 
-import createImage from './createImage';
+type Keys<T> = {
+    [P in keyof T]: true;
+};
 
-const ALLOWED_ATTRIBUTES = Object.keys(
-    createImage({
-        effects: [],
-        filename: '',
-        mime_type: '',
-        original_height: 0,
-        original_width: 0,
-        size: 0,
-        uuid: '',
-        version: 0,
-    }),
-);
+const shape: Keys<ImageNode> = {
+    type: true,
+    file: true,
+    href: true,
+    layout: true,
+    width: true,
+    width_factor: true,
+    children: true,
+};
+
+const ALLOWED_ATTRIBUTES = Object.keys(shape);
 
 const normalizeRedundantImageAttributes = (editor: Editor, [node, path]: NodeEntry): boolean => {
     if (!isImageNode(node)) {
