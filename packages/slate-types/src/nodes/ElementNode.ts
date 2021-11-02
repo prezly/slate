af5a2278dbfb2cc8edfcceef5d8c50940df8a3f1
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-02 16:51:57 +0200

    Decouple `isElementNode` type-guard from depending on `Element` type

--- packages/slate-types/src/nodes/ElementNode.ts
+++ packages/slate-types/src/nodes/ElementNode.ts
@@ -1,9 +1,11 @@
-import type { BaseElement, Element } from 'slate';
+import type { BaseElement } from 'slate';
+
+import { isObject } from '../lib';
 
 export default interface ElementNode extends BaseElement {
     type: string;
 }
 
-export function isElementNode(element: Element): element is ElementNode {
-    return typeof element.type === 'string';
+export function isElementNode(element: unknown): element is ElementNode {
+    return isObject(element) && Array.isArray(element.children) && typeof element.type === 'string';
 }
