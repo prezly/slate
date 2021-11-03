334adf7e51314b6e8dabd5ad35da7e34833158fc
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-03 13:27:38 +0200

    Define node `type` arguments as `ElementNode['type']`
    To make it more clear

--- packages/slate-commons/src/commands/isBlockActive.ts
+++ packages/slate-commons/src/commands/isBlockActive.ts
@@ -1,6 +1,7 @@
+import { ElementNode } from '@prezly/slate-types';
 import { Editor, Element, Location } from 'slate';
 
-const isBlockActive = (editor: Editor, type: string, at?: Location): boolean => {
+const isBlockActive = (editor: Editor, type: ElementNode['type'], at?: Location): boolean => {
     const [match] = Array.from(
         Editor.nodes(editor, {
             match: (node) => Element.isElementType(node, type),
