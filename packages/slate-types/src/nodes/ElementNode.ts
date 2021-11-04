a5f0c5ad35293ba62e5d9160b1cd80876360656e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-04 10:40:30 +0200

    Introduce `isElementNode` function overload
    - to check if value is `ElementNode`
    - to check if value is `ElementNode` of the given type
    - to check if value is `ElementNode` of one of the given types

--- packages/slate-types/src/nodes/ElementNode.ts
+++ packages/slate-types/src/nodes/ElementNode.ts
@@ -6,6 +6,33 @@ export default interface ElementNode extends BaseElement {
     type: string;
 }
 
-export function isElementNode(element: unknown): element is ElementNode {
-    return isObject(element) && Array.isArray(element.children) && typeof element.type === 'string';
+/**
+ * Check if the value is having an `ElementNode` shape: `{ children: array, type: string }`.
+ */
+export function isElementNode(value: unknown): value is ElementNode;
+
+/**
+ * Check if the value is an `ElementNode` of the given type.
+ */
+export function isElementNode<T extends ElementNode>(value: unknown, type: T['type']): value is T;
+
+/**
+ * Check if the value is an `ElementNode` of one of the given types.
+ */
+export function isElementNode<T extends ElementNode>(value: unknown, type: T['type'][]): value is T;
+
+export function isElementNode(value: unknown, type?: string | string[]): boolean {
+    return (
+        isObject(value) &&
+        Array.isArray(value.children) &&
+        typeof value.type === 'string' &&
+        (type === undefined || isElementType(value.type, type))
+    );
+}
+
+function isElementType(elementType: string, checkType: string | string[]): boolean {
+    return (
+        (typeof checkType === 'string' && elementType === checkType) ||
+        (Array.isArray(checkType) && checkType.includes(elementType))
+    );
 }
