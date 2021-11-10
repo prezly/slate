7bc872fe698ee79a4eb6b44cd88ab56c8d9b22c1
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-10 19:36:49 +0200

    Add `Node` type alias

--- packages/slate-types/src/index.ts
+++ packages/slate-types/src/index.ts
@@ -1,4 +1,8 @@
+import { ElementNode, TextNode } from './nodes';
+
 export * from './constants';
 export * from './lib';
 export * from './nodes';
 export * from './sdk';
+
+export type Node = ElementNode | TextNode;
