75ef3b8989e26609ccc3c71f62fad2ee343e771b
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-12 17:48:43 +0200

    Drop unnecessary type-cases
    They are redundant now after the `Node.setNodes()` patch
    has been released.
    
    @see https://github.com/ianstormtaylor/slate/pull/4638

--- packages/slate-editor/src/modules/editor-v4-galleries/transforms/updateGallery.ts
+++ packages/slate-editor/src/modules/editor-v4-galleries/transforms/updateGallery.ts
@@ -1,22 +1,12 @@
 import { GalleryNode, isGalleryNode } from '@prezly/slate-types';
 import { pick } from 'lodash';
-import { Editor, Node, Transforms } from 'slate';
+import { Editor, Transforms } from 'slate';
 
 const updateGallery = (
     editor: Editor,
     changes: Partial<Pick<GalleryNode, 'images' | 'layout' | 'padding' | 'thumbnail_size'>>,
 ) => {
-    /**
-     * The `Partial<Node>` type-cast is a temporary solution until `Transforms.setNodes()` spec is fixed.
-     * @see https://github.com/ianstormtaylor/slate/pull/4638
-     * TODO: Remove the type-cast once the patch is released with the next Slate update.
-     */
-    const changedAttributes = pick(changes, [
-        'images',
-        'layout',
-        'padding',
-        'thumbnail_size',
-    ]) as Partial<Node>;
+    const changedAttributes = pick(changes, ['images', 'layout', 'padding', 'thumbnail_size']);
 
     Transforms.setNodes<GalleryNode>(editor, changedAttributes, {
         match: isGalleryNode,
--- packages/slate-editor/src/modules/editor-v4-image/transforms/setImageHref.ts
+++ packages/slate-editor/src/modules/editor-v4-image/transforms/setImageHref.ts
@@ -1,13 +1,8 @@
 import { ImageNode } from '@prezly/slate-types';
-import { Editor, Node, Range, Transforms } from 'slate';
+import { Editor, Range, Transforms } from 'slate';
 
 const setImageHref = (editor: Editor, at: Range, href: string) => {
-    /**
-     * The `Partial<Node>` type-cast is a temporary solution until `Transforms.setNodes()` spec is fixed.
-     * @see https://github.com/ianstormtaylor/slate/pull/4638
-     * TODO: Remove the type-cast once the patch is released with the next Slate update.
-     */
-    Transforms.setNodes<ImageNode>(editor, { href } as Partial<Node>, { at });
+    Transforms.setNodes<ImageNode>(editor, { href }, { at });
 };
 
 export default setImageHref;
