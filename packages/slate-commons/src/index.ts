443a75fd2d918cfa6148d0e88811e1f7614eb25e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-16 16:58:18 +0200

    Setup multi-project TS repo

--- packages/slate-commons/src/index.ts
+++ packages/slate-commons/src/index.ts
@@ -1,16 +1,3 @@
-import type { ElementNode, TextNode } from '@prezly/slate-types';
-import type { BaseEditor } from 'slate';
-import type { ReactEditor } from 'slate-react';
-import type { HistoryEditor } from 'slate-history';
-
-declare module 'slate' {
-    interface CustomTypes {
-        Editor: BaseEditor & ReactEditor & HistoryEditor;
-        Element: ElementNode;
-        Text: TextNode;
-    }
-}
-
 import * as EditorCommands from './commands';
 
 export { default as EditableWithExtensions } from './EditableWithExtensions';
