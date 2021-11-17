adbacb3016513b83855afef2d44c100f4995dcae
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-17 16:31:08 +0200

    Fix `slate` module ambient types declaration

--- packages/slate-commons/src/index.ts
+++ packages/slate-commons/src/index.ts
@@ -1,3 +1,16 @@
+import type { BaseEditor } from 'slate';
+import type { ReactEditor } from 'slate-react';
+import type { HistoryEditor } from 'slate-history';
+import type { ElementNode, TextNode } from '@prezly/slate-types';
+
+declare module 'slate' {
+    interface CustomTypes {
+        Editor: BaseEditor & ReactEditor & HistoryEditor;
+        Element: ElementNode;
+        Text: TextNode;
+    }
+}
+
 import * as EditorCommands from './commands';
 
 export { default as EditableWithExtensions } from './EditableWithExtensions';
--- packages/slate-lists/src/index.ts
+++ packages/slate-lists/src/index.ts
@@ -1,3 +1,16 @@
+import type { BaseEditor } from 'slate';
+import type { ReactEditor } from 'slate-react';
+import type { HistoryEditor } from 'slate-history';
+import type { ElementNode, TextNode } from '@prezly/slate-types';
+
+declare module 'slate' {
+    interface CustomTypes {
+        Editor: BaseEditor & ReactEditor & HistoryEditor;
+        Element: ElementNode;
+        Text: TextNode;
+    }
+}
+
 export * from './lib';
 export { default as Lists } from './Lists';
 export * from './types';
