38ef2137782a4db8871109343128ec7c0ffb6812
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-24 16:00:10 +0200

    Rewrite watches to gulp
    To add sibling packages sources to watched files.
    Apparently babel is not capable of this.

--- packages/slate-editor/gulpfile.mjs
+++ packages/slate-editor/gulpfile.mjs
@@ -12,16 +12,46 @@ import map from 'gulp-map';
 import rename from 'gulp-rename';
 import postcss from 'gulp-postcss';
 import createSassProcessor from 'gulp-sass';
+import babel from 'gulp-babel';
 
 const sass = createSassProcessor(sassBackend);
 
 const SASS_SOURCES = 'src/**/*.scss';
 const SASS_DECLARATIONS = 'src/styles/**/*.scss';
 const TYPESCRIPT_MODULES_SASS = ['src/**/*.scss', '!src/styles/**/*.scss'];
+const TYPESCRIPT_SOURCES = 'src/**/*.{ts,tsx}';
+const SVG_ICONS = 'src/**/*.svg';
+const DEPENDENCIES_SOURCES = '../{slate-types,slate-hyperscript,slate-commons,slate-lists}/build/**/*.mjs';
 
-/*
- * TASKS
- */
+gulp.task('build:code', function () {
+    return gulp
+        .src(TYPESCRIPT_SOURCES)
+        .pipe(babel({ extends: '../../babel.config.js' }))
+        .pipe(rename((file) => file.extname = '.mjs'))
+        .pipe(gulp.dest('build/esm/'));
+});
+
+gulp.task('watch:code', function () {
+    return gulp
+        .watch([TYPESCRIPT_SOURCES, DEPENDENCIES_SOURCES], gulp.series('build:code'))
+        .on('ready', () => console.log('Watching files'))
+        .on('all', (event, path) => console.log(`[${event}] ${path}`));
+});
+
+gulp.task('build:svg', function () {
+    return gulp
+        .src(SVG_ICONS)
+        .pipe(babel({ extends: '../../babel.config.js' }))
+        .pipe(rename((file) => file.extname = '.svg.mjs'))
+        .pipe(gulp.dest('build/esm/'));
+});
+
+gulp.task('watch:svg', function () {
+    return gulp
+        .watch(TYPESCRIPT_SOURCES, gulp.series('build:svg'))
+        .on('ready', () => console.log('Watching files'))
+        .on('all', (event, path) => console.log(`[${event}] ${path}`));
+});
 
 gulp.task('build:sass', function () {
     return gulp
