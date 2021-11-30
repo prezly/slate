4e13b87f32d0b5c22b360eb88720a3c2134cc236
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-30 16:46:58 +0200

    Fix oembedInfo to comply with type

--- packages/slate-editor/src/modules/tests/data/oembedInfo.ts
+++ packages/slate-editor/src/modules/tests/data/oembedInfo.ts
@@ -9,8 +9,8 @@ export const oembedInfo: OEmbedInfo = {
     description:
         'Prezly provides software for communications teams that want to do more. Manage your media relations, create online newsrooms, and publish and pitch your stories from one place.',
     thumbnail_url: 'https://www.prezly.com/build/images/static/default-social.jpg',
-    thumbnail_width: '819',
-    thumbnail_height: '410',
+    thumbnail_width: 819,
+    thumbnail_height: 410,
     cache_age: 86400,
     html: '<div class="iframely-embed"><div class="iframely-responsive" style="padding-bottom: 50.0611%; padding-top: 120px;"><a href="https://www.prezly.com" data-iframely-url="//cdn.iframe.ly/api/iframe?url=http%3A%2F%2Fprezly.com&amp;key=8fe6cdec03482ac31f27a6ae8ea2fb3f"></a></div></div><script async src="//cdn.iframe.ly/embed.js" charset="utf-8"></script>',
 };
