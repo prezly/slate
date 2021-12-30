85acaddb971065bee3655eef978fb08737669f7f
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-12-30 13:43:06 +0200

    Configure dual build for @prezly/slate-types

--- packages/slate-types/gulpfile.mjs
+++ packages/slate-types/gulpfile.mjs
@@ -4,17 +4,30 @@ import rename from 'gulp-rename';
 
 const TYPESCRIPT_SOURCES = 'src/**/*.{ts,tsx}';
 
-gulp.task('build:code', function () {
+gulp.task('build:esm', function() {
     return gulp
         .src(TYPESCRIPT_SOURCES)
-        .pipe(babel({ extends: '../../babel.config.js' }))
+        .pipe(babel({ extends: '../../babel.esm.config.json' }))
         .pipe(rename((file) => file.extname = '.mjs'))
         .pipe(gulp.dest('build/esm/'));
 });
 
-gulp.task('watch:code', gulp.series('build:code', function () {
+gulp.task('build:cjs', function() {
     return gulp
-        .watch(TYPESCRIPT_SOURCES, gulp.series('build:code'))
-        .on('ready', () => console.log('Watching files'))
-        .on('all', (event, path) => console.log(`[${event}] ${path}`));
-}));
+        .src(TYPESCRIPT_SOURCES)
+        .pipe(babel({ extends: '../../babel.cjs.config.json' }))
+        .pipe(rename((file) => file.extname = '.cjs'))
+        .pipe(gulp.dest('build/cjs/'));
+});
+
+gulp.task('watch:esm', watch(TYPESCRIPT_SOURCES, 'build:esm'));
+gulp.task('watch:cjs', watch(TYPESCRIPT_SOURCES, 'build:cjs'));
+
+function watch(files, build) {
+    return gulp.series(build, () => {
+        return gulp
+            .watch(files, gulp.series(build))
+            .on('ready', () => console.log('Watching files'))
+            .on('all', (event, path) => console.log(`[${event}] ${path}`));
+    });
+}
