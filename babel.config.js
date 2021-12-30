d24f7e8ef0bf7ca5468f42aedc667dab9cea52cd
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-12-30 13:28:38 +0200

    Mark all packages with `type: module`

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,16 @@
-module.exports = (api) => {
-    const isTest = api.env('test');
-
-    return {
-        targets: {
-            esmodules: !isTest,
-            node: '12',
-        },
-        presets: [
-            '@babel/typescript',
-            '@babel/react',
-            '@dr.pogodin/babel-preset-svgr',
-            [
-                '@babel/env',
-                {
-                    useBuiltIns: false,
-                    modules: isTest ? 'commonjs' : false,
-                },
-            ],
-        ],
-        plugins: [
-            ['babel-plugin-transform-remove-imports', { test: '\\.scss$' }],
-            !isTest && ['babel-plugin-add-import-extension', { extension: 'mjs' }],
-        ].filter(Boolean),
-    };
+export default {
+    targets: {
+        esmodules: true,
+        node: '12',
+    },
+    presets: [
+        '@babel/typescript',
+        '@babel/react',
+        '@dr.pogodin/babel-preset-svgr',
+        ['@babel/env', { useBuiltIns: false, modules: false }],
+    ],
+    plugins: [
+        ['babel-plugin-transform-remove-imports', { test: '\\.scss$' }],
+        ['babel-plugin-add-import-extension', { extension: 'mjs' }],
+    ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,17 @@
-module.exports = {
+export default {
     coveragePathIgnorePatterns: ['/node_modules/', '/build/'],
+    extensionsToTreatAsEsm: ['.ts', '.tsx'],
     globals: {
         'ts-jest': {
+            useESM: true,
             tsconfig: 'tsconfig.jest.json',
         },
     },
     moduleNameMapper: {
-        '\\.(scss|svg)$': '<rootDir>/universalMock',
+        '\\.(scss|svg)$': '<rootDir>/universalMock.js',
     },
     preset: 'ts-jest',
     setupFiles: ['<rootDir>/setupTests.js'],
     testEnvironment: 'jsdom',
     testPathIgnorePatterns: ['/node_modules/', '/build/', '<rootDir>/packages/slate-editor/'],
-    transform: {
-        '^.+\\.(mjs|js|jsx|ts|tsx)$': ['babel-jest'],
-    },
 };
