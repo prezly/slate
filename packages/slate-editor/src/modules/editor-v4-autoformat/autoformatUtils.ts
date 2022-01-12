1bdd845807ea1436a670daa5773befc179c3b6ec
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-01-12 13:17:22 +0200

    Drop unused code depending on @plate types

--- packages/slate-editor/src/modules/editor-v4-autoformat/autoformatUtils.ts
+++ packages/slate-editor/src/modules/editor-v4-autoformat/autoformatUtils.ts
@@ -1,36 +1,16 @@
-import type { PlateEditor } from '@udecode/plate-core';
-import { getParent, isElement, isType } from '@udecode/plate-core';
-import { unwrapList, toggleList } from '@udecode/plate-list';
+import { isElementNode } from '@prezly/slate-types';
+import { getParent } from '@udecode/plate-core';
 import type { Editor } from 'slate';
 
-export function clearBlockFormat(editor: Editor): void {
-    unwrapList(editor as PlateEditor);
-}
-
-export const ELEMENT_CODE_BLOCK = 'code_block';
-export const ELEMENT_CODE_LINE = 'code_line';
-
-export function format(editor: Editor, customFormatting: any) {
+export function format(editor: Editor, customFormatting: () => void) {
     if (editor.selection) {
         const parentEntry = getParent(editor, editor.selection);
         if (!parentEntry) return;
         const [node] = parentEntry;
-        if (
-            isElement(node) &&
-            !isType(editor as PlateEditor, node, ELEMENT_CODE_BLOCK) &&
-            !isType(editor as PlateEditor, node, ELEMENT_CODE_LINE)
-        ) {
+        if (isElementNode(node)) {
             customFormatting();
         }
     }
-}
-
-export function formatList(editor: Editor, elementType: string) {
-    format(editor, () =>
-        toggleList(editor as PlateEditor, {
-            type: elementType,
-        }),
-    );
 }
 
 export function formatText(editor: Editor, text: string) {
