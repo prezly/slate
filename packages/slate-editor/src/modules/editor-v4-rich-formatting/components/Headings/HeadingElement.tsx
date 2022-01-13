4425de127a1e55dd725a0e8a1ae4d5844a71e62c
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-01-13 13:01:52 +0200

    Merge HeadingOne and HeadingTwo elements styles

--- packages/slate-editor/src/modules/editor-v4-rich-formatting/components/Headings/HeadingElement.tsx
+++ packages/slate-editor/src/modules/editor-v4-rich-formatting/components/Headings/HeadingElement.tsx
@@ -1,11 +1,11 @@
+import type { HeadingNode } from '@prezly/slate-types';
+import { HEADING_1_NODE_TYPE, HEADING_2_NODE_TYPE } from '@prezly/slate-types';
 import classNames from 'classnames';
 import type { FunctionComponent, HTMLAttributes } from 'react';
 import React from 'react';
 import type { RenderElementProps } from 'slate-react';
 
-import './HeadingOneElement.scss';
-import type { HeadingNode } from '@prezly/slate-types';
-import { HEADING_1_NODE_TYPE, HEADING_2_NODE_TYPE } from '@prezly/slate-types';
+import './HeadingElement.scss';
 
 interface Props extends HTMLAttributes<HTMLHeadingElement> {
     attributes?: RenderElementProps['attributes'];
@@ -24,9 +24,9 @@ export const HeadingElement: FunctionComponent<Props> = ({
         <Heading
             {...attributes}
             {...props}
-            className={classNames(className, {
-                'editor-v4-heading-one-element': element.type === HEADING_1_NODE_TYPE,
-                'editor-v4-heading-two-element': element.type === HEADING_2_NODE_TYPE,
+            className={classNames(className, 'editor-v4-heading-element', {
+                'editor-v4-heading-element--heading-one': element.type === HEADING_1_NODE_TYPE,
+                'editor-v4-heading-element--heading-two': element.type === HEADING_2_NODE_TYPE,
             })}
             data-slate-type={element.type}
             data-slate-value={JSON.stringify(element)}
