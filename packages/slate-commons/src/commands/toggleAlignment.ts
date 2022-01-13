b140a0164d73050bca3f2505806308adb153130e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-01-13 12:05:04 +0200

    [MT-4437] Only apply alignment to top-level elements

--- packages/slate-commons/src/commands/toggleAlignment.ts
+++ packages/slate-commons/src/commands/toggleAlignment.ts
@@ -1,15 +1,19 @@
 import type { AlignableNode, Alignment } from '@prezly/slate-types';
 import { isAlignableElement } from '@prezly/slate-types';
-import type { Editor } from 'slate';
+import type { Editor, Node, Path } from 'slate';
 import { Transforms } from 'slate';
 
 export function toggleAlignment(editor: Editor, align: Alignment | undefined): void {
     if (align === undefined) {
         Transforms.unsetNodes(editor, 'align', {
-            match: isAlignableElement,
+            match: isTopLevelAlignableElement,
         });
         return;
     }
 
-    Transforms.setNodes<AlignableNode>(editor, { align }, { match: isAlignableElement });
+    Transforms.setNodes<AlignableNode>(editor, { align }, { match: isTopLevelAlignableElement });
+}
+
+function isTopLevelAlignableElement(node: Node, path: Path): node is AlignableNode {
+    return path.length === 1 && isAlignableElement(node);
 }
