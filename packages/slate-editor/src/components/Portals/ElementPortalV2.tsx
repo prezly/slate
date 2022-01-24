d508d55605b7ce545db7d8181c3c745c0224a742
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-01-24 14:47:45 +0200

    [MT-4595] Rewrite `FloatingContainer.Container` with ElementPortalV2
    Because `ElementPortalV2` does not support "cover" placement,
    I had to emulate "cover" logic with Popper modifiers.

--- packages/slate-editor/src/components/Portals/ElementPortalV2.tsx
+++ packages/slate-editor/src/components/Portals/ElementPortalV2.tsx
@@ -1,5 +1,4 @@
-import type { FunctionComponent } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
 
 import type { Props as BasePortalV2Props } from './BasePortalV2';
 import { BasePortalV2 } from './BasePortalV2';
@@ -9,12 +8,20 @@ interface Props extends Omit<BasePortalV2Props, 'getBoundingClientRect'> {
 }
 
 /**
- * It's not the same as `ElementPortal` beacuse it uses `react-popper` internally,
- * which supports positioning only outside of the target `element`.
+ * It's not the same as `ElementPortal` because this one uses `react-popper` internally,
+ * which supports positioning only outside the target `element`.
  */
-export const ElementPortalV2: FunctionComponent<Props> = ({ element, ...props }) => (
-    <BasePortalV2
-        {...props}
-        getBoundingClientRect={() => (element ? element.getBoundingClientRect() : null)}
-    />
-);
+export class ElementPortalV2 extends Component<Props> {
+    getBoundingClientRect = () => {
+        return this.props.element?.getBoundingClientRect() || null;
+    };
+
+    render() {
+        const { children, ...props } = this.props;
+        return (
+            <BasePortalV2 {...props} getBoundingClientRect={this.getBoundingClientRect}>
+                {children}
+            </BasePortalV2>
+        );
+    }
+}
