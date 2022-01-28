07e01f1d8e089afe793cbc63f5c438f1436e7dcf
Dmitrii Pikulin <aspirisen@gmail.com>
2022-01-28 21:01:22 +0300

    Fix link focus

---
+++ packages/slate-editor/src/components/Portals/convertClientRect.ts
@@ -0,0 +1,28 @@
+import type { Rect } from 'rangefix';
+
+import { noop } from '#lodash';
+
+export type PureRect = ReturnType<typeof convertClientRect>;
+
+/**
+ * We have to manually re-create a ClientRect-shape object instead of `...rect`,
+ * as `DOMRect` object properties are not enumerable.
+ * @see https://github.com/microsoft/TypeScript/issues/9726
+ */
+export function convertClientRect(domRect: DOMRect | ClientRect | Rect | null) {
+    if (!domRect) {
+        return null;
+    }
+
+    return {
+        top: domRect.top,
+        right: domRect.right,
+        bottom: domRect.bottom,
+        left: domRect.left,
+        width: domRect.width,
+        height: domRect.height,
+        x: 'x' in domRect ? domRect.x : domRect.left,
+        y: 'y' in domRect ? domRect.y : domRect.top,
+        toJSON: noop,
+    };
+}
