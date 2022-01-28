cf8b2b743f5b88495f6bda199c5e5356d313826e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-01-28 11:31:27 +0200

    [MT-4625] Fix improper dependency management for ResizeObserver initialization

--- packages/slate-editor/src/lib/hooks/useResizeObserver.ts
+++ packages/slate-editor/src/lib/hooks/useResizeObserver.ts
@@ -1,12 +1,17 @@
 import { useEffect, useRef } from 'react';
 
+import { useLatest } from './react-use';
+
 export function useResizeObserver(element: HTMLElement | null, callback: ResizeObserverCallback) {
     const observer = useRef<ResizeObserver | null>(null);
+    const props = useLatest({ callback });
 
     useEffect(
         function () {
             if (!observer.current) {
-                observer.current = new ResizeObserver(callback);
+                observer.current = new ResizeObserver(function (...args) {
+                    props.current.callback(...args);
+                });
             }
         },
         [observer],
