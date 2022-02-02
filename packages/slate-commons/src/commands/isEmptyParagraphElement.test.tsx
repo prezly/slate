c87d8b9c63866250d691876bf5f62ebb59363481
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-02-02 12:13:45 +0200

    Fix failing tests

--- packages/slate-commons/src/commands/isEmptyParagraphElement.test.tsx
+++ packages/slate-commons/src/commands/isEmptyParagraphElement.test.tsx
@@ -16,13 +16,13 @@ describe('isEmptyParagraphElement', () => {
                 <h-text />
             </h-p>
             <h-p>
-                <h-text></h-text>
+                <h-text> </h-text>
             </h-p>
             <h-p></h-p>
         </editor>
     ) as unknown as Editor;
 
-    it('Considers paragraph node with text node with text to not be empty', () => {
+    it('Considers paragraph with non-empty text to not be empty', () => {
         expect(isEmptyParagraphElement(editor, editor.children[0])).toBe(false);
     });
 
@@ -30,19 +30,23 @@ describe('isEmptyParagraphElement', () => {
         expect(isEmptyParagraphElement(editor, editor.children[1])).toBe(true);
     });
 
-    it('Considers paragraph node with text node with whitespace to not be empty', () => {
+    it('Considers paragraph node with whitespace to not be empty', () => {
         expect(isEmptyParagraphElement(editor, editor.children[2])).toBe(false);
     });
 
-    it('Considers text node with whitespace to not be empty', () => {
-        expect(isEmptyParagraphElement(editor, editor.children[3])).toBe(false);
+    it('Considers paragraph without text nodes to be empty', () => {
+        expect(isEmptyParagraphElement(editor, editor.children[3])).toBe(true);
     });
 
-    it('Considers paragraph node with whitespace only to be empty when whitespace is ignored', () => {
+    it('Considers paragraph with non-empty text to not be empty even if whitespace is ignored', () => {
+        expect(isEmptyParagraphElement(editor, editor.children[0], { trim: true })).toBe(false);
+    });
+
+    it('Considers paragraph with whitespace only to be empty when whitespace is ignored', () => {
         expect(isEmptyParagraphElement(editor, editor.children[2], { trim: true })).toBe(true);
     });
 
-    it('Considers text node with whitespace only to be empty when whitespace is ignored', () => {
+    it('Considers paragraph without text nodes to be empty when whitespace is ignored', () => {
         expect(isEmptyParagraphElement(editor, editor.children[3], { trim: true })).toBe(true);
     });
 });
