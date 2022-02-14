5cc9de76a274f1ff27c402aceb28124a21aca2a1
Dmitrii Pikulin <aspirisen@gmail.com>
2022-02-14 15:47:13 +0300

    Change class names

--- packages/slate-editor/src/components/Stack/Stack.tsx
+++ packages/slate-editor/src/components/Stack/Stack.tsx
@@ -14,13 +14,57 @@ export interface StackProps {
 
 export function Stack(props: React.PropsWithChildren<StackProps>) {
     return (
-        <div
-            className={classNames(
-                styles.stack,
-                styles[`--${props.direction ?? 'row'}-${props.spacing}`],
-            )}
-        >
+        <div className={classNames(styles.stack, selectSpaceClassName(props))}>
             {props.children}
         </div>
     );
+}
+
+function selectSpaceClassName(props: StackProps) {
+    switch (props.direction) {
+        case 'column': {
+            switch (props.spacing) {
+                case 'none':
+                    return styles['--column-none'];
+                case 'spacing-half':
+                    return styles['--column-spacing-half'];
+                case 'spacing-1':
+                    return styles['--column-spacing-1'];
+                case 'spacing-1-5':
+                    return styles['--column-spacing-1-5'];
+                case 'spacing-2':
+                    return styles['--column-spacing-2'];
+                case 'spacing-3':
+                    return styles['--column-spacing-3'];
+                case 'spacing-4':
+                    return styles['--column-spacing-4'];
+                case 'spacing-5':
+                    return styles['--column-spacing-5'];
+            }
+        }
+
+        case 'row': {
+            switch (props.spacing) {
+                case 'none':
+                    return styles['--row-none'];
+                case 'spacing-half':
+                    return styles['--row-spacing-half'];
+                case 'spacing-1':
+                    return styles['--row-spacing-1'];
+                case 'spacing-1-5':
+                    return styles['--row-spacing-1-5'];
+                case 'spacing-2':
+                    return styles['--row-spacing-2'];
+                case 'spacing-3':
+                    return styles['--row-spacing-3'];
+                case 'spacing-4':
+                    return styles['--row-spacing-4'];
+                case 'spacing-5':
+                    return styles['--row-spacing-5'];
+            }
+        }
+
+        default:
+            return undefined;
+    }
 }
