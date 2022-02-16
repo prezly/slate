26251850263623cdd5d16b661ddd1810325c8614
Dmitrii Pikulin <aspirisen@gmail.com>
2022-02-16 15:46:55 +0300

    Add icons

--- packages/slate-editor/src/components/Toolbox/Toolbox.stories.tsx
+++ packages/slate-editor/src/components/Toolbox/Toolbox.stories.tsx
@@ -3,13 +3,24 @@ import * as React from 'react';
 import type { OptionsGroupOption } from '#components';
 import { VStack } from '#components';
 import { Toolbox, Toggle, OptionsGroup, Button } from '#components';
-import { ExternalLink, Delete, ItemsLayoutVertical, ItemsLayoutHorizontal } from '#icons';
+import {
+    ExternalLink,
+    Delete,
+    ItemsLayoutVertical,
+    ItemsLayoutHorizontal,
+    ImageLayoutContained,
+    ImageLayoutExpanded,
+    ImageLayoutFullWidth,
+    ImageSpacingNarrow,
+    ImageSpacingRegular,
+    ImageSpacingWide,
+} from '#icons';
 
 export default {
     title: 'Components/Toolbox',
 };
 
-export function Base() {
+export function WebBookmark() {
     const [cardLayout, setCardLayout] = React.useState<
         typeof cardLayoutOptions[number]['value'] | undefined
     >('vertical');
@@ -65,6 +76,84 @@ export function Base() {
                 <Toolbox.Footer>
                     <Button variant="clear" Icon={Delete} fullWidth transparent="0-5">
                         Remove web bookmark
+                    </Button>
+                </Toolbox.Footer>
+            </Toolbox.Panel>
+        </div>
+    );
+}
+
+export function GallerySettings() {
+    const [cardLayout, setCardLayout] = React.useState<
+        typeof galleryWidthOptions[number]['value'] | undefined
+    >('contained');
+
+    const [spacingBetweenImages, setSpacingBetweenImages] = React.useState<
+        typeof spacingBetweenImagesOptions[number]['value'] | undefined
+    >('regular');
+
+    const galleryWidthOptions: OptionsGroupOption<'contained' | 'expanded' | 'full-width'>[] = [
+        {
+            value: 'contained',
+            label: 'Contained',
+            icon: (props) => <ImageLayoutContained fill={props.isActive ? '#F9CA7B' : 'white'} />,
+        },
+        {
+            value: 'expanded',
+            label: 'Expanded',
+            icon: (props) => <ImageLayoutExpanded fill={props.isActive ? '#F9CA7B' : 'white'} />,
+        },
+        {
+            value: 'full-width',
+            label: 'Full width',
+            icon: (props) => <ImageLayoutFullWidth fill={props.isActive ? '#F9CA7B' : 'white'} />,
+        },
+    ];
+
+    const spacingBetweenImagesOptions: OptionsGroupOption<'narrow' | 'regular' | 'wide'>[] = [
+        {
+            value: 'narrow',
+            label: 'Narrow',
+            icon: (props) => <ImageSpacingNarrow fill={props.isActive ? '#F9CA7B' : 'white'} />,
+        },
+        {
+            value: 'regular',
+            label: 'Regular',
+            icon: (props) => <ImageSpacingRegular fill={props.isActive ? '#F9CA7B' : 'white'} />,
+        },
+        {
+            value: 'wide',
+            label: 'Wide',
+            icon: (props) => <ImageSpacingWide fill={props.isActive ? '#F9CA7B' : 'white'} />,
+        },
+    ];
+
+    return (
+        <div style={{ width: 280 }}>
+            <Toolbox.Panel>
+                <Toolbox.Header withCloseButton>Gallery settings</Toolbox.Header>
+
+                <Toolbox.Section caption="Gallery width">
+                    <OptionsGroup
+                        name="gallery-width"
+                        options={galleryWidthOptions}
+                        selectedValue={cardLayout}
+                        onChange={setCardLayout}
+                    />
+                </Toolbox.Section>
+
+                <Toolbox.Section caption="Card layout">
+                    <OptionsGroup
+                        name="spacing-between-images"
+                        options={spacingBetweenImagesOptions}
+                        selectedValue={spacingBetweenImages}
+                        onChange={setSpacingBetweenImages}
+                    />
+                </Toolbox.Section>
+
+                <Toolbox.Footer>
+                    <Button variant="clear" Icon={Delete} fullWidth transparent="0-5">
+                        Remove gallery
                     </Button>
                 </Toolbox.Footer>
             </Toolbox.Panel>
