01603b25a9bacfb495fa1c0f14956382a0bc177d
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-02-23 19:00:07 +0200

    [MT-4655] Make inline links selected on click

--- packages/slate-editor/src/modules/editor-v4-inline-links/components/LinkElement.tsx
+++ packages/slate-editor/src/modules/editor-v4-inline-links/components/LinkElement.tsx
@@ -1,7 +1,8 @@
 import type { LinkNode } from '@prezly/slate-types';
-import type { FunctionComponent } from 'react';
 import React from 'react';
+import { Editor, Transforms } from 'slate';
 import type { RenderElementProps } from 'slate-react';
+import { ReactEditor, useSlateStatic } from 'slate-react';
 
 import { LinkWithTooltip } from '#modules/editor-v4-components';
 
@@ -11,28 +12,43 @@ interface Props extends RenderElementProps {
     element: LinkNode;
 }
 
-export const LinkElement: FunctionComponent<Props> = ({ attributes, children, element }) => (
-    // Using a wrapping `span` to avoid a rare Slate bug which occurs as
-    // a failed `ReactEditor.toSlateNode` in Slate's Editable onClick handler.
-    // For more details, see https://github.com/prezly/prezly/pull/8016#discussion_r454190469
-    <span {...attributes} data-slate-type={element.type} data-slate-value={JSON.stringify(element)}>
-        <LinkWithTooltip href={element.href}>
-            {({ ariaAttributes, onHide, onShow, setReferenceElement }) => (
-                <a
-                    {...ariaAttributes}
-                    className={styles.link}
-                    href={element.href}
-                    onBlur={onHide}
-                    onFocus={onShow}
-                    onMouseEnter={onShow}
-                    onMouseLeave={onHide}
-                    ref={setReferenceElement}
-                    rel="noreferrer"
-                    target={element.new_tab ? '_blank' : undefined}
-                >
-                    {children}
-                </a>
-            )}
-        </LinkWithTooltip>
-    </span>
-);
+export function LinkElement({ attributes, children, element }: Props) {
+    const editor = useSlateStatic();
+
+    function onClick() {
+        const path = ReactEditor.findPath(editor, element);
+        const range = Editor.range(editor, path);
+        Transforms.select(editor, range);
+    }
+
+    return (
+        // Using a wrapping `span` to avoid a rare Slate bug which occurs as
+        // a failed `ReactEditor.toSlateNode` in Slate's Editable onClick handler.
+        // For more details, see https://github.com/prezly/prezly/pull/8016#discussion_r454190469
+        <span
+            {...attributes}
+            data-slate-type={element.type}
+            data-slate-value={JSON.stringify(element)}
+        >
+            <LinkWithTooltip href={element.href}>
+                {({ ariaAttributes, onHide, onShow, setReferenceElement }) => (
+                    <a
+                        {...ariaAttributes}
+                        className={styles.link}
+                        href={element.href}
+                        onBlur={onHide}
+                        onFocus={onShow}
+                        onClick={onClick}
+                        onMouseEnter={onShow}
+                        onMouseLeave={onHide}
+                        ref={setReferenceElement}
+                        rel="noreferrer"
+                        target={element.new_tab ? '_blank' : undefined}
+                    >
+                        {children}
+                    </a>
+                )}
+            </LinkWithTooltip>
+        </span>
+    );
+}
