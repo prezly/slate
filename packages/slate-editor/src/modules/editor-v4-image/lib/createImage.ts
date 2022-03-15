a8a5bac097ca4889e1eb5987a59d0f079913efc1
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-03-15 18:04:39 +0200

    Refactor `createImage` function

--- packages/slate-editor/src/modules/editor-v4-image/lib/createImage.ts
+++ packages/slate-editor/src/modules/editor-v4-image/lib/createImage.ts
@@ -1,17 +1,16 @@
 import type { ImageNode } from '@prezly/slate-types';
 import { ImageLayout, IMAGE_NODE_TYPE } from '@prezly/slate-types';
-import type { UploadcareImageStoragePayload } from '@prezly/uploadcare';
 
-export function createImage(
-    file: UploadcareImageStoragePayload,
-    {
-        children = [{ text: '' }],
-        href = '',
-        layout = ImageLayout.CONTAINED,
-        width = '100%',
-        width_factor = '100%',
-    }: Partial<Omit<ImageNode, 'file' | 'type'>>,
-): ImageNode {
+type Parameters = Partial<Omit<ImageNode, 'file' | 'type'>> & Pick<ImageNode, 'file'>;
+
+export function createImage({
+    file,
+    href = '',
+    layout = ImageLayout.CONTAINED,
+    width = '100%',
+    width_factor = '100%',
+    children = [{ text: '' }],
+}: Parameters): ImageNode {
     return {
         type: IMAGE_NODE_TYPE,
         children,
