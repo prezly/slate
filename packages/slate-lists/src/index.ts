adca040dc0ec259f62d44974fb64798352345f16
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-04-13 20:14:09 +0300

    Fix conflicting exports in slate-lists

--- packages/slate-lists/src/index.ts
+++ packages/slate-lists/src/index.ts
@@ -1,4 +1,4 @@
-export * as ListsEditor from './lib';
+export * as Lists from './lib';
 export * as Normalizations from './normalizations';
 export * from './types';
 export { withLists } from './withLists';
