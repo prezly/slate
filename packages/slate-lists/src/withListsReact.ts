5bba069c8e1658e970b76373c08d7c137d253302
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-04-13 19:12:15 +0300

    Fix withListsReact type declaration

--- packages/slate-lists/src/withListsReact.ts
+++ packages/slate-lists/src/withListsReact.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 
+import type { Editor } from 'slate';
 import type { ReactEditor } from 'slate-react';
 
 import { withRangeCloneContentsPatched } from './util';
@@ -8,7 +9,7 @@ import { withRangeCloneContentsPatched } from './util';
  * Enables Range.prototype.cloneContents monkey patch to improve pasting behavior
  * in few edge cases.
  */
-export function withListsReact<T extends ReactEditor>(editor: T): T {
+export function withListsReact<T extends Editor & ReactEditor>(editor: T): T {
     const { setFragmentData } = editor;
 
     editor.setFragmentData = (data: DataTransfer) => {
