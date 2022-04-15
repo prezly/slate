222e96f024c83f08a68c3627bbf419c93f8d1631
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-04-15 12:19:07 +0300

    Fix TS typing

--- packages/slate-commons/src/commands/isMarkActive.ts
+++ packages/slate-commons/src/commands/isMarkActive.ts
@@ -2,6 +2,6 @@ import type { Text } from 'slate';
 import { Editor } from 'slate';
 
 export function isMarkActive<T extends Text>(editor: Editor, mark: keyof Omit<T, 'text'>): boolean {
-    const marks = Editor.marks(editor) as Record<typeof mark, boolean>;
+    const marks = Editor.marks(editor) as Record<keyof T, boolean>;
     return marks ? marks[mark] === true : false;
 }
