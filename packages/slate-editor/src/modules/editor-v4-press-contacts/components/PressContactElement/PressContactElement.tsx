4ef193c8fc182de208f0e42326fdd8f8ee547832
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-11 19:02:31 +0300

    Convert `PressContactElement` to `EditorBlock`

--- packages/slate-editor/src/modules/editor-v4-press-contacts/components/PressContactElement/PressContactElement.tsx
+++ packages/slate-editor/src/modules/editor-v4-press-contacts/components/PressContactElement/PressContactElement.tsx
@@ -1,11 +1,8 @@
 import type { ContactNode } from '@prezly/slate-types';
-import classNames from 'classnames';
-import type { FunctionComponent } from 'react';
 import React from 'react';
 import type { RenderElementProps } from 'slate-react';
-import { useSelected } from 'slate-react';
 
-import { Avatar } from '#components';
+import { Avatar, EditorBlock } from '#components';
 import { User } from '#icons';
 
 import { JobDescription } from '../JobDescription';
@@ -17,55 +14,47 @@ interface Props extends RenderElementProps {
     element: ContactNode;
 }
 
-export const PressContactElement: FunctionComponent<Props> = ({
-    attributes,
-    children,
-    element,
-}) => {
-    const isSelected = useSelected();
-
+export function PressContactElement({ attributes, children, element }: Props) {
     return (
-        <div
+        <EditorBlock
             {...attributes}
-            className={classNames('editor-v4-press-contact-element', {
-                'editor-v4-press-contact-element--active': isSelected,
-            })}
-            data-slate-type={element.type}
-            data-slate-value={JSON.stringify(element)}
-        >
-            <div className="editor-v4-press-contact-element__wrapper" contentEditable={false}>
-                {element.contact.avatar_url && (
-                    <Avatar
-                        className="editor-v4-press-contact-element__avatar"
-                        name={element.contact.name}
-                        size="large"
-                        square
-                        src={element.contact.avatar_url}
-                    />
-                )}
+            element={element}
+            renderBlock={() => (
+                <div className="editor-v4-press-contact-element__wrapper" contentEditable={false}>
+                    {element.contact.avatar_url && (
+                        <Avatar
+                            className="editor-v4-press-contact-element__avatar"
+                            name={element.contact.name}
+                            size="large"
+                            square
+                            src={element.contact.avatar_url}
+                        />
+                    )}
 
-                {!element.contact.avatar_url && (
-                    <div className="editor-v4-press-contact-element__avatar">
-                        <User className="editor-v4-press-contact-element__placeholder" />
+                    {!element.contact.avatar_url && (
+                        <div className="editor-v4-press-contact-element__avatar">
+                            <User className="editor-v4-press-contact-element__placeholder" />
+                        </div>
+                    )}
+
+                    <div className="editor-v4-press-contact-element__content">
+                        <h3 className="editor-v4-press-contact-element__name">
+                            {element.contact.name}
+                        </h3>
+
+                        <JobDescription
+                            className="editor-v4-press-contact-element__job-description"
+                            contact={element.contact}
+                        />
+
+                        <SocialFields contact={element.contact} />
                     </div>
-                )}
-
-                <div className="editor-v4-press-contact-element__content">
-                    <h3 className="editor-v4-press-contact-element__name">
-                        {element.contact.name}
-                    </h3>
-
-                    <JobDescription
-                        className="editor-v4-press-contact-element__job-description"
-                        contact={element.contact}
-                    />
-
-                    <SocialFields contact={element.contact} />
                 </div>
-            </div>
-
+            )}
+            void
+        >
             {/* We have to render children or Slate will fail when trying to find the node. */}
             {children}
-        </div>
+        </EditorBlock>
     );
-};
+}
