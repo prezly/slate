ec3b0556aeb702a9a3573da81338be153a499284
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-16 17:35:49 +0300

    Improve our `scrollTo` polyfill

--- packages/slate-editor/src/lib/scrollTo.ts
+++ packages/slate-editor/src/lib/scrollTo.ts
@@ -4,7 +4,11 @@
  * A polyfill for `HTMLElement.prototype.scrollTo`, which does not work in old browsers:
  * https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo#Browser_Compatibility
  */
-export function scrollTo(element: HTMLElement, x: number, y: number) {
-    element.scrollLeft = x;
-    element.scrollTop = y;
+export function scrollTo(element: HTMLElement, left: number, top: number) {
+    if ('scroll' in element) {
+        element.scroll({ left, top })
+    } else {
+        element.scrollLeft = left;
+        element.scrollTop = top;
+    }
 }
