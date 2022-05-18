ac4c6015f64867e63e900975965460e46ae7e242
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-18 16:38:46 +0300

    Convert FloatingContainer Button styles to SCSS module

--- packages/slate-editor/src/modules/editor-v4-components/FloatingContainer/components/Button/Button.tsx
+++ packages/slate-editor/src/modules/editor-v4-components/FloatingContainer/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import React, { forwardRef } from 'react';
 
 import { Add } from '#icons';
 
-import './Button.scss';
+import styles from './Button.module.scss';
 
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
     onClick: MouseEventHandler<HTMLButtonElement>;
@@ -16,9 +16,9 @@ export const Button = forwardRef<HTMLButtonElement, Props>(
     ({ className, open, onClick, variant = 'default', ...props }, ref: Ref<HTMLButtonElement>) => (
         <button
             {...props}
-            className={classNames('editor-v4-floating-container-button', className, {
-                'editor-v4-floating-container-button--open': open,
-                'editor-v4-floating-container-button--variant-green': variant === 'green',
+            className={classNames(styles.Button, className, {
+                [styles.open]: open,
+                [styles.green]: variant === 'green',
             })}
             onMouseDown={(event) => {
                 // We cannot use `onClick` because the button captures focus,
@@ -31,7 +31,7 @@ export const Button = forwardRef<HTMLButtonElement, Props>(
             tabIndex={-1}
             type="button"
         >
-            <Add className="editor-v4-floating-container-button__icon" />
+            <Add className={styles.icon} />
         </button>
     ),
 );
