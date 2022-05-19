312e660f02621e9b2851a71e8327acb442fafaf0
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-19 14:43:28 +0300

    Convert ImageSizeWarning styles to SCSS module

--- packages/slate-editor/src/components/ImageSizeWarning/ImageSizeWarning.tsx
+++ packages/slate-editor/src/components/ImageSizeWarning/ImageSizeWarning.tsx
@@ -1,10 +1,9 @@
 import classNames from 'classnames';
-import type { FunctionComponent } from 'react';
 import React from 'react';
 
 import * as TooltipV2 from '../TooltipV2';
 
-import './ImageSizeWarning.scss';
+import styles from './ImageSizeWarning.module.scss';
 
 export interface Props {
     className?: string;
@@ -13,18 +12,20 @@ export interface Props {
 const WARNING =
     'Your image has higher resolution than recommended and might not be rendered properly or even may not be rendered at all.';
 
-export const ImageSizeWarning: FunctionComponent<Props> = ({ className }) => (
-    <TooltipV2.Tooltip tooltip={WARNING}>
-        {({ ariaAttributes, onHide, onShow, setReferenceElement }) => (
-            <div
-                {...ariaAttributes}
-                className={classNames('image-size-warning', className)}
-                onMouseEnter={onShow}
-                onMouseLeave={onHide}
-                ref={setReferenceElement}
-            >
-                <i className="icon icon-warning2" />
-            </div>
-        )}
-    </TooltipV2.Tooltip>
-);
+export function ImageSizeWarning({ className }: Props) {
+    return (
+        <TooltipV2.Tooltip tooltip={WARNING}>
+            {({ ariaAttributes, onHide, onShow, setReferenceElement }) => (
+                <div
+                    {...ariaAttributes}
+                    className={classNames(styles.ImageSizeWarning, className)}
+                    onMouseEnter={onShow}
+                    onMouseLeave={onHide}
+                    ref={setReferenceElement}
+                >
+                    <i className="icon icon-warning2" />
+                </div>
+            )}
+        </TooltipV2.Tooltip>
+    );
+}
