d4a205b287c8c97fd9ff74f0828e11fe02b61540
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-06-03 16:59:49 +0300

    Bring back `useImage` returning the blob `url` string
    I just feel bad completely discarding it :)

--- packages/slate-editor/src/lib/hooks/useImage.ts
+++ packages/slate-editor/src/lib/hooks/useImage.ts
@@ -4,15 +4,18 @@ import { fetchImageWithProgress } from '../fetchImageWithProgress';
 
 import { useAsyncProgress } from './useAsyncProgress';
 
+type OriginalURIOrBlobDataURI = string;
+
 interface State {
     error?: Error;
     loading: boolean;
     progress: number;
     loaded: boolean;
+    url: OriginalURIOrBlobDataURI | undefined;
 }
 
 export function useImage(src: string): State {
     const fetchImage = useCallback(() => fetchImageWithProgress(src), [src]);
     const { error, loading, progress, value } = useAsyncProgress(fetchImage);
-    return { error, loading, progress, loaded: Boolean(value) };
+    return { error, loading, progress, loaded: Boolean(value), url: value };
 }
