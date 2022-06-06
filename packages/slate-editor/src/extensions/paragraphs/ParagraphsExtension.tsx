9c563ec2c075e61dc4b733976dd8b79e2abebbd7
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-06-07 01:49:40 +0300

    Inline ParagraphsExtension deserialization config

--- packages/slate-editor/src/extensions/paragraphs/ParagraphsExtension.tsx
+++ packages/slate-editor/src/extensions/paragraphs/ParagraphsExtension.tsx
@@ -1,21 +1,37 @@
 import type { Extension } from '@prezly/slate-commons';
+import { createDeserializeElement } from '@prezly/slate-commons';
 import { isParagraphNode, PARAGRAPH_NODE_TYPE } from '@prezly/slate-types';
 import React from 'react';
 import type { RenderElementProps } from 'slate-react';
 
 import { ParagraphElement } from './components';
-import { deserialize } from './deserialize';
 import {
+    createParagraph,
     normalizeOrphanText,
     normalizeRedundantParagraphAttributes,
     normalizeUnknownElement,
+    parseSerializedElement,
 } from './lib';
 
 export const EXTENSION_ID = 'ParagraphsExtension';
 
 export const ParagraphsExtension = (): Extension => ({
     id: EXTENSION_ID,
-    deserialize,
+    deserialize: {
+        element: {
+            [PARAGRAPH_NODE_TYPE]: createDeserializeElement(parseSerializedElement),
+            BR: () => createParagraph(),
+            DIV: () => createParagraph(),
+            FONT: () => createParagraph(),
+            H1: () => createParagraph(), // FIXME
+            H2: () => createParagraph(), // FIXME
+            H3: () => createParagraph(), // FIXME
+            H4: () => createParagraph(), // FIXME
+            H5: () => createParagraph(), // FIXME
+            H6: () => createParagraph(), // FIXME
+            P: () => createParagraph(),
+        },
+    },
     normalizeNode: [
         normalizeOrphanText,
         normalizeRedundantParagraphAttributes,
