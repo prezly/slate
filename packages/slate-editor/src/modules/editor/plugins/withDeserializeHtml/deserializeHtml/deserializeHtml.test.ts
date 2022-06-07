43576a89374d00ef93ec470832acd28fa03005d4
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-06-07 17:45:55 +0300

    Reorganize HTML pasting test cases fixtures

--- packages/slate-editor/src/modules/editor/plugins/withDeserializeHtml/deserializeHtml/deserializeHtml.test.ts
+++ packages/slate-editor/src/modules/editor/plugins/withDeserializeHtml/deserializeHtml/deserializeHtml.test.ts
@@ -16,53 +16,53 @@ function readTestFile(filepath: string): string {
 function handleError() {}
 
 describe('deserializeHtml', () => {
-    it('Wraps orphan "list-items" into "lists"', () => {
+    it('should wrap orphan "list-items" into "lists"', () => {
         // https://github.com/prezly/prezly/pull/8292#discussion_r473031425
-        const input = readTestFile('input/wraps-orphan-list-items-into-lists.html');
-        const expected = readTestFile('expected/wraps-orphan-list-items-into-lists.json');
+        const input = readTestFile('01.wraps-orphan-list-items-into-lists.html');
+        const expected = readTestFile('01.wraps-orphan-list-items-into-lists.json');
 
         expect(deserializeHtml(extensions, input, handleError)).toMatchObject(JSON.parse(expected));
     });
 
-    it('Wraps orphan "list-item-texts" into "lists"', () => {
+    it('should wrap orphan "list-item-texts" into "lists"', () => {
         // https://github.com/prezly/prezly/pull/8359#discussion_r478198175
-        const input = readTestFile('input/wraps-orphan-list-item-texts-into-lists.html');
-        const expected = readTestFile('expected/wraps-orphan-list-item-texts-into-lists.json');
+        const input = readTestFile('02.wraps-orphan-list-item-texts-into-lists.html');
+        const expected = readTestFile('02.wraps-orphan-list-item-texts-into-lists.json');
 
         expect(deserializeHtml(extensions, input, handleError)).toMatchObject(JSON.parse(expected));
     });
 
-    it('Removes orphan newlines in "paragraphs"', () => {
-        const input = readTestFile('input/paragraphs-with-newlines.html');
-        const expected = readTestFile('expected/paragraphs-with-newlines.json');
+    it('should remove orphan newlines in "paragraphs"', () => {
+        const input = readTestFile('03.paragraphs-with-newlines.html');
+        const expected = readTestFile('03.paragraphs-with-newlines.json');
 
         expect(deserializeHtml(extensions, input, handleError)).toMatchObject(JSON.parse(expected));
     });
 
-    it('Deserializes a "paragraph"', () => {
-        const input = readTestFile('input/paragraph.html');
-        const expected = readTestFile('expected/paragraph.json');
+    it('it should deserialize a "paragraph"', () => {
+        const input = readTestFile('04.paragraph.html');
+        const expected = readTestFile('04.paragraph.json');
 
         expect(deserializeHtml(extensions, input, handleError)).toMatchObject(JSON.parse(expected));
     });
 
-    it('Deserializes a nested "divider" (Google Docs)', () => {
-        const input = readTestFile('input/google-docs-divider.html');
-        const expected = readTestFile('expected/google-docs-divider.json');
+    it('it should deserialize a nested "divider" (Google Docs)', () => {
+        const input = readTestFile('05.google-docs-divider.html');
+        const expected = readTestFile('05.google-docs-divider.json');
 
         expect(deserializeHtml(extensions, input, handleError)).toMatchObject(JSON.parse(expected));
     });
 
-    it('Deserializes a mix of "paragraphs" and "quotes"', () => {
-        const input = readTestFile('input/paragraphs-and-quotes.html');
-        const expected = readTestFile('expected/paragraphs-and-quotes.json');
+    it('it should deserialize a mix of "paragraphs" and "quotes"', () => {
+        const input = readTestFile('06.paragraphs-and-quotes.html');
+        const expected = readTestFile('06.paragraphs-and-quotes.json');
 
         expect(deserializeHtml(extensions, input, handleError)).toMatchObject(JSON.parse(expected));
     });
 
-    it('Deserializes nested "lists"', () => {
-        const input = readTestFile('input/nested-lists.html');
-        const expected = readTestFile('expected/nested-lists.json');
+    it('it should deserialize nested "lists"', () => {
+        const input = readTestFile('07.nested-lists.html');
+        const expected = readTestFile('07.nested-lists.json');
 
         expect(deserializeHtml(extensions, input, handleError)).toMatchObject(JSON.parse(expected));
     });
