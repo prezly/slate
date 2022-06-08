2cbb4b901c342fe12800a3388f6799f957b415fa
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-06-08 16:41:48 +0300

    Drop broken exports

--- packages/slate-commons/src/lib/index.ts
+++ packages/slate-commons/src/lib/index.ts
@@ -1,6 +1,4 @@
 export { createDeserializeElement } from './createDeserializeElement';
-export { decodeSlateFragment } from './decodeSlateFragment';
-export { encodeSlateFragment } from './encodeSlateFragment';
 export { isGoogleDocsWrapper } from './isGoogleDocsWrapper';
 export { nodeIdManager } from './nodeIdManager';
 export { useSavedSelection } from './useSavedSelection';
