81c7a289cdb2cf0dd10d0148b54a504b66ffd6f2
Dmitrii Pikulin <aspirisen@gmail.com>
2022-06-17 17:37:46 +0300

    Fix prettier

--- packages/slate-editor/src/modules/nodes-hierarchy/types.ts
+++ packages/slate-editor/src/modules/nodes-hierarchy/types.ts
@@ -1,7 +1,7 @@
 import type { Editor, Path, NodeEntry } from 'slate';
 
 export type HierarchyNormalizer = (editor: Editor, path: Path) => boolean;
-export type HierarchyNodeQuery = (entry: NodeEntry, editor: Editor) => boolean
+export type HierarchyNodeQuery = (entry: NodeEntry, editor: Editor) => boolean;
 export type HierarchyFixer = (editor: Editor, entry: NodeEntry) => boolean;
 
 export const EDITOR_NODE_TYPE = Symbol('EDITOR');
