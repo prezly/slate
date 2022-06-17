89482935ca3d93b4b85e571c9c5ce2c7b0b50c38
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-06-17 18:42:14 +0300

    Bring back `ref` support to ListExtension elements
    Refs are required by Slate in order to locate document
    nodes by their DOM elements.

--- packages/slate-editor/src/extensions/list/components/ListItemTextElement.tsx
+++ packages/slate-editor/src/extensions/list/components/ListItemTextElement.tsx
@@ -1,18 +1,22 @@
 import type { ListItemTextNode } from '@prezly/slate-types';
 import classNames from 'classnames';
 import type { HTMLAttributes } from 'react';
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import styles from './ListItemTextElement.module.scss';
 
-interface Props extends HTMLAttributes<HTMLDivElement> {
+interface Props extends HTMLAttributes<HTMLSpanElement> {
     element: ListItemTextNode;
 }
 
-export function ListItemTextElement({ children, className, ...props }: Props) {
-    return (
-        <span {...props} className={classNames(styles.ListItemTextElement, className)}>
-            {children}
-        </span>
-    );
-}
+export const ListItemTextElement = forwardRef<HTMLSpanElement, Props>(
+    ({ children, className, ...props }, ref) => {
+        return (
+            <span {...props} className={classNames(styles.ListItemTextElement, className)} ref={ref}>
+                {children}
+            </span>
+        );
+    },
+);
+
+ListItemTextElement.displayName = 'ListItemTextElement';
