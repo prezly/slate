e3288c28e4c07bd149bbcdfe421f1ecf3b0b9fa2
pestaa <istvan@prezly.com>
2022-06-23 21:09:18 +0200

    [MT-5031] Reading should not be considered change in form state

--- packages/slate-editor/src/modules/editor/lib/isEditorValueEqual.ts
+++ packages/slate-editor/src/modules/editor/lib/isEditorValueEqual.ts
@@ -3,5 +3,5 @@ import type { Value } from '../types';
 import { serialize } from './serialize';
 
 export function isEditorValueEqual(a: Value, b: Value): boolean {
-    return a === b && serialize(a) === serialize(b);
+    return a === b || serialize(a) === serialize(b);
 }
