85727aeb03f23b3a63a586d60c19bd47751da7f4
Dmitrii Pikulin <aspirisen@gmail.com>
2022-07-06 20:35:06 +0300

    Add unset mark

---
+++ packages/slate-commons/src/commands/unsetMark.ts
@@ -0,0 +1,12 @@
+import { Transforms } from 'slate';
+import type { Text } from 'slate';
+import type { Editor } from 'slate';
+
+export function unsetMark<T extends Text>(editor: Editor, node: T, mark: keyof Omit<T, 'text'>) {
+    if (node[mark]) {
+        Transforms.unsetNodes<T>(editor, mark.toString(), { match: (n) => n === node });
+        return true;
+    }
+
+    return false;
+}
