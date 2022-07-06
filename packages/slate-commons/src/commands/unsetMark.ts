ba86e6c7edb6cc08ad3fc9293e42db624a120854
Dmitrii Pikulin <aspirisen@gmail.com>
2022-07-06 21:50:34 +0300

    Add tests for isinside

--- packages/slate-commons/src/commands/unsetMark.ts
+++ packages/slate-commons/src/commands/unsetMark.ts
@@ -4,7 +4,10 @@ import type { Editor } from 'slate';
 
 export function unsetMark<T extends Text>(editor: Editor, node: T, mark: keyof Omit<T, 'text'>) {
     if (node[mark]) {
-        Transforms.unsetNodes<T>(editor, mark.toString(), { match: (n) => n === node });
+        Transforms.unsetNodes<T>(editor, mark.toString(), {
+            match: (n) => n === node,
+        });
+
         return true;
     }
 
--- packages/slate-editor/src/modules/nodes-hierarchy/queries/isAllowedInTableCell.ts
+++ packages/slate-editor/src/modules/nodes-hierarchy/queries/isAllowedInTableCell.ts
@@ -1,6 +1,7 @@
 import { isListNode, isParagraphNode } from '@prezly/slate-types';
-import type { Node } from 'slate';
+import type { Node} from 'slate';
+import { Text } from 'slate';
 
 export function isAllowedInTableCell(node: Node) {
-    return isParagraphNode(node) || isListNode(node);
+    return isParagraphNode(node) || isListNode(node) || Text.isText(node);
 }
