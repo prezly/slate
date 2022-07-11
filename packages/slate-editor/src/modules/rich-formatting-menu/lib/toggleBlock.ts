bc83b68362be6fc490e637a2a725caf0373ed7a1
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-07-11 17:13:12 +0300

    Update usages of `Lists` namespace to the new name

--- packages/slate-editor/src/modules/rich-formatting-menu/lib/toggleBlock.ts
+++ packages/slate-editor/src/modules/rich-formatting-menu/lib/toggleBlock.ts
@@ -1,5 +1,5 @@
 import { EditorCommands } from '@prezly/slate-commons';
-import { Lists, ListType } from '@prezly/slate-lists';
+import { ListsEditor, ListType } from '@prezly/slate-lists';
 import type { ElementNode } from '@prezly/slate-types';
 import { BULLETED_LIST_NODE_TYPE, NUMBERED_LIST_NODE_TYPE } from '@prezly/slate-types';
 import type { Editor } from 'slate';
@@ -13,18 +13,18 @@ export function toggleBlock<T extends ElementNode>(editor: Editor, type: T['type
     }
 
     if (type === BULLETED_LIST_NODE_TYPE) {
-        Lists.wrapInList(editor, ListType.UNORDERED);
-        Lists.setListType(editor, ListType.UNORDERED);
+        ListsEditor.wrapInList(editor, ListType.UNORDERED);
+        ListsEditor.setListType(editor, ListType.UNORDERED);
         return;
     }
 
     if (type === NUMBERED_LIST_NODE_TYPE) {
-        Lists.wrapInList(editor, ListType.ORDERED);
-        Lists.setListType(editor, ListType.ORDERED);
+        ListsEditor.wrapInList(editor, ListType.ORDERED);
+        ListsEditor.setListType(editor, ListType.ORDERED);
         return;
     }
 
-    Lists.unwrapList(editor);
+    ListsEditor.unwrapList(editor);
 
     if (path && EditorCommands.isCursorInEmptyParagraph(editor, { trim: true })) {
         EditorCommands.removeChildren(editor, [currentNode, path]);
