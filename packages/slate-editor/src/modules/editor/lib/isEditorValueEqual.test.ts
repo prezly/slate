ce69611e12bff7388480cdcffad852a27f9ad0f7
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-07-15 15:51:32 +0300

    Extend `isValueEqual` test to cover pre-serialization cleanup

--- packages/slate-editor/src/modules/editor/lib/isEditorValueEqual.test.ts
+++ packages/slate-editor/src/modules/editor/lib/isEditorValueEqual.test.ts
@@ -1,3 +1,5 @@
+import { withoutNodes } from '@prezly/slate-commons';
+import { isQuoteNode } from '@prezly/slate-types';
 import { createEditor as createSlateEditor } from 'slate';
 
 import { createEditor } from '#modules/editor';
@@ -20,6 +22,23 @@ describe('slate-editor - isEditorValueEqual', () => {
                 type: 'paragraph',
             },
         ];
+
+        expect(isEditorValueEqual(editor, a, b)).toBe(true);
+    });
+
+    it('should run pre-serialization cleanup from extensions', () => {
+        const editor = createEditor(createSlateEditor(), () => [
+            {
+                id: 'PreSerializationCleanup',
+                serialize: (nodes) => withoutNodes(nodes, isQuoteNode),
+            },
+        ]);
+
+        const a = [
+            { type: 'paragraph', children: [{ text: 'A wise man once said:' }] },
+            { type: 'block-quote', children: [{ text: 'Hello' }] },
+        ];
+        const b = [{ type: 'paragraph', children: [{ text: 'A wise man once said:' }] }];
 
         expect(isEditorValueEqual(editor, a, b)).toBe(true);
     });
