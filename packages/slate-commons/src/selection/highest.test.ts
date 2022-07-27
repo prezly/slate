0f97ca00b5a4ffc99489bb75e9bd137d36c00e06
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-07-27 19:07:47 +0300

    Change `Selection.highest()` to always return `Path`

--- packages/slate-commons/src/selection/highest.test.ts
+++ packages/slate-commons/src/selection/highest.test.ts
@@ -6,20 +6,17 @@ describe('highest', function () {
         expect(highest([5])).toEqual([5]);
     });
 
-    it('should return top-level point value for a given point', function () {
-        expect(highest({ path: [2, 4, 5], offset: 10 })).toEqual({ path: [2], offset: 0 });
-        expect(highest({ path: [5], offset: 1 })).toEqual({ path: [5], offset: 0 });
+    it('should return top-level path value for a given point', function () {
+        expect(highest({ path: [2, 4, 5], offset: 10 })).toEqual([2]);
+        expect(highest({ path: [5], offset: 1 })).toEqual([5]);
     });
 
-    it('should return top-level range value for a given range', function () {
+    it('should return top-level focus point path for a given range', function () {
         expect(
             highest({
                 anchor: { path: [2, 4, 5], offset: 10 },
                 focus: { path: [5, 2], offset: 5 },
             }),
-        ).toEqual({
-            anchor: { path: [2], offset: 0 },
-            focus: { path: [5], offset: 0 },
-        });
+        ).toEqual([5]);
     });
 });
