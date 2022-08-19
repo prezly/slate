7ed5724395b77aa50a4cb6210c78a5a4f1f846d7
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-08-19 13:28:53 +0300

    [QRF-24] Wrap `unwrapList` code into `withoutNormalizing`
    To make sure normalizations do not kick in during transformations.
    This reveals the problem that leads to QRF-24

--- packages/slate-lists/src/lib/unwrapList.ts
+++ packages/slate-lists/src/lib/unwrapList.ts
@@ -1,3 +1,5 @@
+import { Editor } from "slate";
+
 import type { ListsEditor } from '../types';
 
 import { decreaseDepth } from './decreaseDepth';
@@ -5,17 +7,20 @@ import { getListItemsInRange } from './getListItemsInRange';
 
 /**
  * Unwraps all "list-items" in the current selection.
- * No list be left in the current selection.
+ * No lists will be left in the current selection.
  */
 export function unwrapList(editor: ListsEditor): void {
-    let iterations = 0;
-    while (getListItemsInRange(editor, editor.selection).length > 0) {
-        iterations++;
+    Editor.withoutNormalizing(editor, () => {
+        let iterations = 0;
 
-        decreaseDepth(editor);
+        while (getListItemsInRange(editor, editor.selection).length > 0) {
+            iterations++;
 
-        if (iterations > 1000) {
-            throw new Error('Too many iterations. Most likely there is a bug causing an infinite loop.');
+            decreaseDepth(editor);
+
+            if (iterations > 1000) {
+                throw new Error('Too many iterations. Most likely there is a bug causing an infinite loop.');
+            }
         }
-    }
+    });
 }
