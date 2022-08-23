9ba554ebba5c1e1d6f49fa8ad72ba49591da73ee
Dmitrii Pikulin <aspirisen@gmail.com>
2022-08-23 17:36:33 +0300

    Fix pasting into image caption

--- packages/slate-editor/src/extensions/paste-slate-content/lib/withSlatePasting.ts
+++ packages/slate-editor/src/extensions/paste-slate-content/lib/withSlatePasting.ts
@@ -1,5 +1,6 @@
-import type { Editor } from 'slate';
-import { Transforms } from 'slate';
+import { EditorCommands } from '@prezly/slate-commons';
+import { isImageNode } from '@prezly/slate-types';
+import { Editor, Transforms } from 'slate';
 
 import { createDataTransfer, decodeSlateFragment } from '#lib';
 
@@ -15,6 +16,12 @@ export function withSlatePasting<T extends Editor>(editor: T) {
             const fragment = decodeSlateFragment(slateFragment);
 
             if (isValidFragment(fragment)) {
+                const [[imageNode]] = Editor.nodes(editor, { match: isImageNode });
+                if (imageNode && EditorCommands.isNodeEmpty(editor, imageNode)) {
+                    Transforms.insertNodes(editor, fragment, { at: editor.selection?.anchor.path });
+                    return;
+                }
+
                 if (editor.selection) {
                     Transforms.insertFragment(editor, fragment);
                 } else {
