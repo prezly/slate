9fdc15d05e8b7f4098d5ba14ca3556707c9ee81e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-09-22 16:51:34 +0300

    [DEV-8820] Fix CloseButton interaction warnings

--- packages/slate-editor/src/components/EditorBlock/CloseButton.tsx
+++ packages/slate-editor/src/components/EditorBlock/CloseButton.tsx
@@ -1,7 +1,9 @@
 import classNames from 'classnames';
+import type { MouseEventHandler } from 'react';
 import React, { type ButtonHTMLAttributes } from 'react';
 
 import { Cross } from '#icons';
+import { useFunction } from '#lib';
 
 import styles from './CloseButton.module.scss';
 
@@ -9,9 +11,18 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     onClick?: () => void;
 }
 
-export function CloseButton({ className, ...props }: Props) {
+export function CloseButton({ className, onClick, ...props }: Props) {
+    const handleClick = useFunction<MouseEventHandler<HTMLButtonElement>>((event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        onClick?.();
+    });
     return (
-        <button {...props} className={classNames(styles.CloseButton, className)}>
+        <button
+            {...props}
+            onClick={onClick ? handleClick : undefined}
+            className={classNames(styles.CloseButton, className)}
+        >
             <Cross className={styles.Icon} />
         </button>
     );
