d649126c89478e7bfee37c4c96a1984a584e498b
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-09-23 14:13:38 +0300

    [DEV-8813] Implement `GalleryPlaceholderElement`

--- packages/slate-editor/src/extensions/placeholders/components/index.ts
+++ packages/slate-editor/src/extensions/placeholders/components/index.ts
@@ -1,4 +1,5 @@
 export { AttachmentPlaceholderElement } from './AttachmentPlaceholderElement';
 export { ImagePlaceholderElement } from './ImagePlaceholderElement';
+export { GalleryPlaceholderElement } from './GalleryPlaceholderElement';
 export { PlaceholderElement } from './PlaceholderElement';
 export { Placeholder } from './Placeholder';
