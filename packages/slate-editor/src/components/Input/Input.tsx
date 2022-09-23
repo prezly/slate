f668efbf4456d44da0a89dd34eac6bcfee2b91cb
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-09-23 19:29:00 +0300

    [DEV-8862] Rewrite `Input` element states to JS-driven classnames

--- packages/slate-editor/src/components/Input/Input.tsx
+++ packages/slate-editor/src/components/Input/Input.tsx
@@ -1,36 +1,72 @@
 import classNames from 'classnames';
-import * as React from 'react';
 import type { ChangeEvent, InputHTMLAttributes } from 'react';
+import * as React from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { WarningTriangle } from '#icons';
 import { useFunction } from '#lib';
 
 import styles from './Input.module.scss';
 
-interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+export interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
     value: string;
     onChange: (newValue: string, valid: boolean) => void;
     icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 }
 
-export function Input({ className, icon: Icon, onChange, ...attributes }: Props) {
+export function Input({
+    className,
+    disabled,
+    icon: Icon,
+    onChange,
+    onBlur,
+    onFocus,
+    value,
+    ...attributes
+}: Props) {
+    const input = useRef<HTMLInputElement>(null);
+
+    const [valid, setValid] = useState(true);
+    const [focused, setFocused] = useState(false);
+
     const handleChange = useFunction((event: ChangeEvent<HTMLInputElement>) => {
         const { value, validity } = event.currentTarget;
+        setValid(validity.valid);
         onChange(value, validity.valid);
     });
 
+    useEffect(() => {
+        setValid(input.current?.validity.valid ?? true);
+    }, [value]);
+
     return (
-        <label className={styles.Wrapper}>
+        <label
+            className={classNames(styles.InputBox, {
+                [styles.disabled]: disabled,
+                [styles.invalid]: !valid,
+                [styles.focused]: focused,
+                [styles.withIcon]: Icon !== undefined,
+            })}
+        >
             <input
                 {...attributes}
-                className={classNames(className, styles.Input, {
-                    [styles.withIcon]: Icon !== undefined,
-                })}
+                ref={input}
+                className={classNames(className, styles.Input)}
+                disabled={disabled}
+                value={value}
                 onChange={handleChange}
+                onFocus={(event) => {
+                    setFocused(true);
+                    onFocus?.(event);
+                }}
+                onBlur={(event) => {
+                    setFocused(false);
+                    onBlur?.(event);
+                }}
             />
             {Icon && <Icon className={styles.Icon} />}
 
-            <WarningTriangle className={styles.InvalidIcon} />
+            <WarningTriangle className={styles.WarningIcon} />
         </label>
     );
 }
