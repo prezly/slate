40b113bd7c3ed184392a5d2d1c814ac7681338de
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-09-26 17:28:11 +0300

    [DEV-8862] Add Storybook entry for `PlaceholderElement`

---
+++ packages/slate-editor/src/extensions/placeholders/components/PlaceholderElement.stories.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import { createEditor as createSlateEditor } from 'slate';
+import { type RenderElementProps, Slate } from 'slate-react';
+
+import { PlaceholderAttachment } from '#icons';
+
+import { PlaceholdersExtension } from '#extensions/placeholders';
+import { createEditor } from '#modules/editor';
+
+import { PlaceholderNode } from '../PlaceholderNode';
+
+import { PlaceholderElement } from './PlaceholderElement';
+
+const extensions = [PlaceholdersExtension()];
+const editor = createEditor(createSlateEditor(), () => extensions);
+
+const placeholder: PlaceholderNode = {
+    type: PlaceholderNode.Type.ATTACHMENT,
+    uuid: 'e57a4e5c-7769-4cbd-a159-a68be9373d26',
+    children: [{ text: '' }],
+};
+
+const attributes: RenderElementProps['attributes'] = {
+    'data-slate-node': 'element',
+    'data-slate-void': true,
+    ref: () => null,
+};
+
+export default {
+    title: 'Extensions/Placeholders/components/PlaceholderElement',
+    decorators: [
+        (Story: React.ComponentType) => (
+            <Slate editor={editor} value={[placeholder]}>
+                <div style={{ width: 680, boxSizing: 'border-box' }}>
+                    <Story />
+                </div>
+            </Slate>
+        ),
+    ],
+};
+
+export function Default() {
+    return (
+        <PlaceholderElement
+            attributes={attributes}
+            element={placeholder}
+            icon={PlaceholderAttachment}
+            title="Click to upload an attachment"
+            description="Supported formats: pdf, .ppt, Keynote, .zip, .doc, etc. - Max. 25MB"
+        >
+            {''}
+        </PlaceholderElement>
+    );
+}
+
+export function DropZone() {
+    return (
+        <PlaceholderElement
+            attributes={attributes}
+            element={placeholder}
+            icon={PlaceholderAttachment}
+            title="Drag or click to upload an attachment"
+            description="Supported formats: pdf, .ppt, Keynote, .zip, .doc, etc. - Max. 25MB"
+            onDrop={() => null}
+        >
+            {''}
+        </PlaceholderElement>
+    );
+}
