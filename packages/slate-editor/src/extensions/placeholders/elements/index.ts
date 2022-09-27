fe6cda422290b56ca8a1aad229c1e5da8f8a9313
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-09-27 11:47:59 +0300

    [DEV-8816] Implement `SocialPostPlaceholderElement`

--- packages/slate-editor/src/extensions/placeholders/elements/index.ts
+++ packages/slate-editor/src/extensions/placeholders/elements/index.ts
@@ -2,5 +2,6 @@ export { AttachmentPlaceholderElement } from './AttachmentPlaceholderElement';
 export { EmbedPlaceholderElement } from './EmbedPlaceholderElement';
 export { GalleryPlaceholderElement } from './GalleryPlaceholderElement';
 export { ImagePlaceholderElement } from './ImagePlaceholderElement';
+export { SocialPostPlaceholderElement } from './SocialPostPlaceholderElement';
 export { VideoPlaceholderElement } from './VideoPlaceholderElement';
 export { WebBookmarkPlaceholderElement } from './WebBookmarkPlaceholderElement';
