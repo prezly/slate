3824399232264f99430eb45b058a382ecad2566e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-09-27 12:44:06 +0300

    [DEV-8917] Add an empty paragraph after every placeholder inserted

--- packages/slate-editor/src/extensions/placeholders/lib/insertPlaceholders.ts
+++ packages/slate-editor/src/extensions/placeholders/lib/insertPlaceholders.ts
@@ -1,14 +1,20 @@
+import { EditorCommands } from '@prezly/slate-commons';
 import { times } from 'lodash-es';
 import type { Editor } from 'slate';
 
 import type { PlaceholderNode } from '../PlaceholderNode';
 
-import { insertPlaceholder } from './insertPlaceholder';
+import { createPlaceholder } from './createPlaceholder';
 
 export function insertPlaceholders<T extends PlaceholderNode.Type>(
     editor: Editor,
     count: number,
     props: Partial<PlaceholderNode<T>> & Pick<PlaceholderNode<T>, 'type'>,
+    ensureEmptyParagraphAfter = false,
 ): PlaceholderNode<T>[] {
-    return times(count, () => insertPlaceholder<T>(editor, props));
+    const placeholders = times(count, () => createPlaceholder<T>(props));
+
+    EditorCommands.insertNodes(editor, placeholders, { ensureEmptyParagraphAfter });
+
+    return placeholders;
 }
