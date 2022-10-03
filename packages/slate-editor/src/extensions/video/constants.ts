53059d6d7210e30fd92521624cbac9cc5d2ef359
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-10-03 14:00:44 +0300

    [DEV-8957] Only keep video file types supported by Uploadcare

--- packages/slate-editor/src/extensions/video/constants.ts
+++ packages/slate-editor/src/extensions/video/constants.ts
@@ -1,1 +1,5 @@
-export const VIDEO_TYPES = ['video/mp4', 'video/quicktime', 'video/ogg', 'video/webm'];
+/**
+ * Video types supported by Uploadcare
+ * @see https://uploadcare.com/docs/transformations/video-encoding/
+ */
+export const VIDEO_TYPES = ['video/mp4', 'video/ogg', 'video/webm'];
