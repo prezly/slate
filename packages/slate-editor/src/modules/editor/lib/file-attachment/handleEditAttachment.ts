a29e4eb671117b215010512e0ebdb6091e1f9139
aspirisen <aspirisen@gmail.com>
2022-11-18 15:04:33 +0400

    Forbid '/' in file names

--- packages/slate-editor/src/modules/editor/lib/file-attachment/handleEditAttachment.ts
+++ packages/slate-editor/src/modules/editor/lib/file-attachment/handleEditAttachment.ts
@@ -15,16 +15,18 @@ export async function handleEditAttachment(editor: Editor, element: Partial<Atta
         return;
     }
 
-    Transforms.setNodes<AttachmentNode>(editor, element, {
+    const newElement = { ...currentFileAttachment, ...element };
+
+    newElement.file.filename = newElement.file.filename.replaceAll('/', ':');
+
+    Transforms.setNodes<AttachmentNode>(editor, newElement, {
         match: isAttachmentNode,
     });
 
-    const { description, file } = { ...currentFileAttachment, ...element };
-
     EventsEditor.dispatchEvent(editor, 'attachment-edited', {
-        description: description,
-        mimeType: file.mime_type,
-        size: file.size,
-        uuid: file.uuid,
+        description: newElement.description,
+        mimeType: newElement.file.mime_type,
+        size: newElement.file.size,
+        uuid: newElement.file.uuid,
     });
 }
