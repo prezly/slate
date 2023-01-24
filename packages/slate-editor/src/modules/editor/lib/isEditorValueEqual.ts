947aa88ba25bb5f2dc01bc60523eef7965d0fc94
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-01-24 13:43:59 +0200

    [DEV-9877] Extract cache management into a separate layer
    To not mix it with the business logic

--- packages/slate-editor/src/modules/editor/lib/isEditorValueEqual.ts
+++ packages/slate-editor/src/modules/editor/lib/isEditorValueEqual.ts
@@ -3,7 +3,65 @@ import type { Editor, Element, Text, Descendant } from 'slate';
 
 import type { Value } from '../types';
 
+export function isEditorValueEqual(editor: Editor, a: Value, b: Value): boolean {
+    const compareNodes = cached(editor, (node: Descendant, another: Descendant): boolean => {
+        const isNodeText = isText(node);
+        const isAnotherText = isText(another);
+
+        if (isNodeText && isAnotherText) {
+            return isEqual(node, another); // default lodash `isEqual` logic;
+        }
+
+        if (!isNodeText && !isAnotherText) {
+            const equal = editor.isElementEqual(node as Element, another as Element);
+            if (typeof equal !== 'undefined') {
+                return equal && compareLists(node.children, another.children);
+            }
+            const { children, ...props } = node;
+            const { children: otherChildren, ...otherProps } = another;
+
+            return isEqual(props, otherProps) && compareLists(children, otherChildren);
+        }
+
+        return false; // comparing Text & Element === always false
+    });
+
+    function compareLists(nodes: Descendant[], other: Descendant[]): boolean {
+        return (
+            nodes.length == other.length && nodes.every((node, i) => compareNodes(node, other[i]))
+        );
+    }
+
+    return a === b || compareLists(editor.serialize(a), editor.serialize(b));
+}
+
+function isText(node: Descendant): node is Text {
+    return 'text' in node;
+}
+
+// CACHE
+
+const CACHE: WeakMap<Editor, WeakMap<Descendant, WeakMap<Descendant, boolean>>> = new WeakMap();
+
 type WeakMatrix = WeakMap<Editor, WeakMap<Descendant, WeakMap<Descendant, boolean>>>;
+type NodesComparator = (node: Descendant, another: Descendant) => boolean;
+
+function cached(editor: Editor, fn: NodesComparator): NodesComparator {
+    return (node, another) => {
+        const cached = get(CACHE, editor, node, another) ?? get(CACHE, editor, another, node);
+
+        if (typeof cached !== 'undefined') {
+            return cached;
+        }
+
+        const ret = fn(node, another);
+
+        set(CACHE, editor, node, another, ret);
+        set(CACHE, editor, another, node, ret);
+
+        return ret;
+    };
+}
 
 function get(
     matrix: WeakMatrix,
@@ -28,61 +86,4 @@ function set(
     level3.set(another, value);
 
     return value;
-}
-
-const CACHE: WeakMap<Editor, WeakMap<Descendant, WeakMap<Descendant, boolean>>> = new WeakMap();
-
-export function isEditorValueEqual(editor: Editor, a: Value, b: Value): boolean {
-    function compareLists(nodes: Descendant[], other: Descendant[]): boolean {
-        return (
-            nodes.length == other.length && nodes.every((node, i) => compareNodes(node, other[i]))
-        );
-    }
-
-    function compareNodes(node: Descendant, another: Descendant): boolean | undefined {
-        const cached = get(CACHE, editor, node, another) ?? get(CACHE, editor, another, node);
-
-        if (typeof cached !== 'undefined') {
-            return cached;
-        }
-
-        const isNodeText = isText(node);
-        const isAnotherText = isText(another);
-
-        if (isNodeText && isAnotherText) {
-            const ret = isEqual(node, another); // default lodash `isEqual` logic;
-
-            set(CACHE, editor, node, another, ret);
-            set(CACHE, editor, another, node, ret);
-
-            return ret;
-        }
-
-        if (!isNodeText && !isAnotherText) {
-            const equal = editor.isElementEqual(node as Element, another as Element);
-            if (typeof equal !== 'undefined') {
-                return equal && compareLists(node.children, another.children);
-            }
-            const { children, ...props } = node;
-            const { children: otherChildren, ...otherProps } = another;
-
-            const ret = isEqual(props, otherProps) && compareLists(children, otherChildren);
-
-            set(CACHE, editor, node, another, ret);
-            set(CACHE, editor, another, node, ret);
-
-            return ret;
-        }
-
-        set(CACHE, editor, node, another, false);
-        set(CACHE, editor, another, node, false);
-
-        return false; // comparing Text & Element === always false
-    }
-
-    return a === b || compareLists(editor.serialize(a), editor.serialize(b));
-}
-
-function isText(node: Descendant): node is Text {
-    return 'text' in node;
 }
