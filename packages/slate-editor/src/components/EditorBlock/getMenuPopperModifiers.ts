5be208c51a9ddb1a913668be55eb677389721357
aspirisen <aspirisen@gmail.com>
2023-02-09 20:00:06 +0400

    Add offset comment

--- packages/slate-editor/src/components/EditorBlock/getMenuPopperModifiers.ts
+++ packages/slate-editor/src/components/EditorBlock/getMenuPopperModifiers.ts
@@ -38,6 +38,17 @@ export function getMenuPopperModifiers(
                     const popperTallerThanReference = popper.height - referenceHeight;
                     const offset = popperTallerThanReference / 2;
 
+                    /**
+                     * If offset is 0 then the menu wil be centered (in the middle) on the reference element.
+                     * For example if the reference element is 150px tall and the menu is 100px tall,
+                     * then the menu will have 25px offset (padding) on top and bottom.
+                     * If the menu is 150px tall, then the reference element will have so to say 25px offset (padding) on top and bottom.
+                     * So when the offset is 0 the menu element will be placed in the middle of the reference element despite what is taller.
+                     * However, we need to snap the menu to the top of the reference element.
+                     * 
+                     * If the menu is taller than the reference element, then we need to "push down" the menu by 25px and `offset` will be positive.
+                     * If the menu is shorter than the reference element, then we need to "pull up" the menu by 25px and `offset` will be negative.
+                     */
                     return [offset, 16];
                 },
             },
@@ -47,8 +58,7 @@ export function getMenuPopperModifiers(
             enabled: true,
             options: {
                 // The order of these properties is important! The first one that has enough space to fit the popup will be used as fallback
-                // We prioritise flipping on Y axis (as this is the most common reason for overflow), then flipping X axis if needed.
-                // `right-start` is there as a fallback for cases when non-standard placement option is used.
+                // We prioritize flipping on Y axis (as this is the most common reason for overflow), then flipping X axis if needed.
                 fallbackPlacements: ['right', 'left'],
             },
         },
