a35dc2316a2cfd1722a7e0b60a4b428e107acd3d
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-05-03 17:48:24 +0300

    [DEV-9918] Make sure `storybook` is using the `react` package installed in the workspace root

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,20 +1,12 @@
-const OMIT_DEPENDENCIES = [
-    'react',
-    'react-dom',
-    'slate',
-    'slate-react',
-    'slate-history',
-];
+const OMIT_DEPENDENCIES = ['react', 'react-dom', 'slate', 'slate-react', 'slate-history'];
 
 function readPackage(pkg, context) {
-    if (pkg.name && pkg.name.startsWith("@prezly/slate-")) {
-        OMIT_DEPENDENCIES.forEach((dependencyName) => {
-            if (pkg.peerDependencies && pkg.peerDependencies[dependencyName]) {
-                pkg.peerDependencies = omitDependency(pkg.peerDependencies, dependencyName);
-                context.log(`Skipping "${dependencyName}" from "${pkg.name}" peerDependencies.`);
-            }
-        });
-    }
+    OMIT_DEPENDENCIES.forEach((dependencyName) => {
+        if (pkg.peerDependencies && pkg.peerDependencies[dependencyName]) {
+            pkg.peerDependencies = omitDependency(pkg.peerDependencies, dependencyName);
+            context.log(`Skipping "${dependencyName}" from "${pkg.name}" peerDependencies.`);
+        }
+    });
 
     return pkg;
 }
@@ -25,13 +17,11 @@ function readPackage(pkg, context) {
  * @returns {Record<string,string>}
  */
 function omitDependency(dependencies, omit) {
-    return Object.fromEntries(
-        Object.entries(dependencies).filter(([name]) => name !== omit),
-    )
+    return Object.fromEntries(Object.entries(dependencies).filter(([name]) => name !== omit));
 }
 
 module.exports = {
     hooks: {
-        readPackage
-    }
+        readPackage,
+    },
 };
