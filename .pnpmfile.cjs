8c91168dee34316e7c18d0f6ded8285a41a16534
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-05-03 18:46:16 +0300

    [DEV-9918] Make sure we use the same @types/react as in the workspace root

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,10 +1,19 @@
-const OMIT_DEPENDENCIES = ['react', 'react-dom', 'slate', 'slate-react', 'slate-history'];
+const OMIT_DEPENDENCIES = ['@types/react', 'react', 'react-dom', 'slate', 'slate-react', 'slate-history'];
 
 function readPackage(pkg, context) {
+    if (pkg.name === '@prezly/slate') {
+        // do not modify the workspace root package.json
+        return pkg;
+    }
+
     OMIT_DEPENDENCIES.forEach((dependencyName) => {
         if (pkg.peerDependencies && pkg.peerDependencies[dependencyName]) {
             pkg.peerDependencies = omitDependency(pkg.peerDependencies, dependencyName);
             context.log(`Skipping "${dependencyName}" from "${pkg.name}" peerDependencies.`);
+        }
+        if (pkg.dependencies && pkg.dependencies[dependencyName]) {
+            pkg.dependencies = omitDependency(pkg.dependencies, dependencyName);
+            context.log(`Skipping "${dependencyName}" from "${pkg.name}" dependencies.`);
         }
     });
 
