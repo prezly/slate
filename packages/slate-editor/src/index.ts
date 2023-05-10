d220c698c6a2ecdabd3bbe1c12c26480fe48d630
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-05-10 23:06:03 +0300

    Merge type overrides back into index.ts

--- packages/slate-editor/src/index.ts
+++ packages/slate-editor/src/index.ts
@@ -1,5 +1,3 @@
-import './types';
-
 export * from '@prezly/slate-types';
 export * from './components';
 export * as Icons from './icons';
@@ -16,3 +14,35 @@ export {
 export { createContactNode, JobDescription } from './extensions/press-contacts';
 export type { User } from './extensions/user-mentions';
 export { type ResultPromise, type UploadcareOptions, withUploadcare } from './modules/uploadcare';
+
+// Editor type
+
+import type { ElementNode, ParagraphNode, TextNode } from '@prezly/slate-types';
+import type { BaseEditor } from 'slate';
+import type { HistoryEditor } from 'slate-history';
+import type { ReactEditor } from 'slate-react';
+
+import type { FlashEditor } from '#extensions/flash-nodes';
+import type {
+    DefaultTextBlockEditor,
+    ElementsEqualityCheckEditor,
+    RichBlocksAwareEditor,
+    SerializingEditor,
+} from '#modules/editor';
+
+type Editor = BaseEditor &
+    ReactEditor &
+    HistoryEditor &
+    DefaultTextBlockEditor<ParagraphNode> &
+    ElementsEqualityCheckEditor &
+    RichBlocksAwareEditor &
+    SerializingEditor &
+    FlashEditor;
+
+declare module 'slate' {
+    interface CustomTypes {
+        Editor: Editor;
+        Element: ElementNode;
+        Text: TextNode;
+    }
+}
