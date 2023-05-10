4cdc15ce4d0e76d699d3092ce90a103d023a6d34
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-05-10 22:39:16 +0300

    Split large mocha bootstrap file into smaller portions

--- .mocha/config.cjs
+++ .mocha/config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
     'extension': ['ts', 'tsx'],
     'require': [
-        `${__dirname}/bootstrap.cjs`, // bootstrap mocha environment
+        `${__dirname}/register/globals.cjs`, // bootstrap mocha environment
+        `${__dirname}/register/babel.cjs`, // bootstrap mocha environment
+        `${__dirname}/register/tsconfig-paths.cjs`, // bootstrap mocha environment
         'ignore-styles', // ignore non-code imports (.css, .scss, .svg, etc)
         'global-jsdom/register', // mock JSDom
         'mocha-expect-snapshot', // toMatchSnapshot
--- .mocha/register/babel.cjs
+++ .mocha/register/babel.cjs
@@ -1,31 +1,6 @@
-const { expect } = require('expect');
 const { getPackagesSync } = require('@manypkg/get-packages');
 
-// Attach sinon-provided assertions to expect()
-const sinon = require('sinon');
-// Expose `jest` expect() function globally
-global.expect = (function extendExpect() {
-    expect.extend({
-        toBeCalled: toJestFormat((actual) => sinon.assert.called(actual)),
-        toBeCalledOnce: toJestFormat((actual) => sinon.assert.calledOnce(actual)),
-        toBeCalledTimes: toJestFormat((actual, times) => sinon.assert.callCount(actual, times)),
-    });
-
-    return expect;
-
-    function toJestFormat(callback) {
-        return function(...args) {
-            try {
-                callback(...args);
-            } catch (error) {
-                return { pass: false, message: error.message };
-            }
-            return { pass: true };
-        };
-    }
-})();
-
-const workspace = getPackagesSync(`${__dirname}/..`);
+const workspace = getPackagesSync(`${__dirname}/../..`);
 
 // Register Babel loader to transpile TS/TSX files
 require('@babel/register').default({
