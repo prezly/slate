79c68cb64a66cc85df42a9ac429d89e2e07fec2c
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-05-10 15:54:02 +0300

    Drop unnecessary typedef file

--- packages/slate-editor/src/@types/popper-max-size-modifier.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'popper-max-size-modifier' {
-    import type { Modifier } from 'react-popper';
-    declare const maxSize: Modifier<'maxSize', Modifier<'preventOverflow'>['options']>;
-    export default maxSize;
-}
