5d37376a9e700e947fc0d5b7f05bda65ce47d863
Oleg Semyonov <oleg@prezly.com>
2023-06-02 14:24:14 +0400

    Refactor: cleanup notifications after they've been responded to

--- packages/slate-editor/src/extensions/placeholders/PlaceholdersManager.ts
+++ packages/slate-editor/src/extensions/placeholders/PlaceholdersManager.ts
@@ -194,21 +194,35 @@ function notify<T extends Type>(type: T, uuid: Uuid, callback: (follower: Follow
     state.notifications = [...state.notifications, notification];
 
     setTimeout(() => {
-        state.notifications = state.notifications.filter((n) => n !== notification);
+        cleanupNotification(notification);
     }, NOTIFICATION_TTL);
 
     state.followers.forEach((follower) => {
         if (is(follower, type, uuid)) {
-            callback(follower as any as Follower<T>);
+            respondToNotification(follower, notification);
         }
     });
 }
 
+function cleanupNotification<T extends Type>(notification: Notification<T>) {
+    state.notifications = state.notifications.filter(
+        (n) => n !== (notification as any as Notification<Type>),
+    );
+}
+
+function respondToNotification<T extends Type>(
+    follower: Follower<T>,
+    notification: Notification<T>,
+) {
+    if (is(follower, notification.type, notification.uuid)) {
+        notification.callback(follower);
+        cleanupNotification(notification);
+    }
+}
+
 function consumeNotifications<T extends Type>(follower: Follower<T>) {
-    state.notifications.forEach(({ type, uuid, callback }) => {
-        if (is(follower, type, uuid)) {
-            (callback as any as Notification<T>['callback'])(follower);
-        }
+    state.notifications.forEach((notification) => {
+        respondToNotification(follower, notification as any as Notification<T>);
     });
 }
 
