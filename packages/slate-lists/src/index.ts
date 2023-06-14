c90ddf7cc2a033121fb3e8b2b397c12ea4054de6
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-06-15 02:00:58 +0300

    [CARE-1802] Add a new export: `withListsNormalization` to aid typical use-cases

--- packages/slate-lists/src/index.ts
+++ packages/slate-lists/src/index.ts
@@ -3,4 +3,5 @@ export { normalizeNode } from './normalizeNode';
 export { onKeyDown } from './onKeyDown';
 export { ListType, type ListsSchema } from './types';
 export { withLists } from './withLists';
+export { withListsNormalization } from './withListsNormalization';
 export { withListsReact } from './withListsReact';
