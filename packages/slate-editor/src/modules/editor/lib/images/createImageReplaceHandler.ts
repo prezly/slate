ef84f07f9f60fd66612e52baf7f378d818b76c21
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-06-29 13:46:38 +0300

    [CARE-1797] Fix TS compilation error

--- packages/slate-editor/src/modules/editor/lib/images/createImageReplaceHandler.ts
+++ packages/slate-editor/src/modules/editor/lib/images/createImageReplaceHandler.ts
@@ -43,7 +43,7 @@ export function createImageReplaceHandler(params: ImageExtensionConfiguration) {
                         ...imageNode,
                         file: image.toPrezlyStoragePayload(),
                     },
-                    operation: 'edit',
+                    operation: 'edit' as const,
                 };
             }),
         );
