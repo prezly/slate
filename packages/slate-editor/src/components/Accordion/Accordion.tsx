435e21b9167a7906a5ced0dd6a6474788d0fb7b1
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-08-16 17:16:19 +0300

    Force menu repositioning when accordion opens

--- packages/slate-editor/src/components/Accordion/Accordion.tsx
+++ packages/slate-editor/src/components/Accordion/Accordion.tsx
@@ -1,21 +1,33 @@
 import classNames from 'classnames';
-import React, { useState, type ReactNode } from 'react';
+import React, { useState, type ReactNode, useEffect } from 'react';
 
 import { Caret } from '#icons';
+import { useLatest } from '#lib';
 
 import styles from './Accordion.module.scss';
 
 interface Props {
     title: string;
     children: ReactNode;
+    onExpand?: () => void;
+    onCollapse?: () => void;
 }
 
-export function Accordion({ title, children }: Props) {
+export function Accordion({ title, children, onExpand, onCollapse }: Props) {
     const [isOpen, setIsOpen] = useState(false);
+    const callbacks = useLatest({ onExpand, onCollapse });
 
     function toggleOpen() {
         setIsOpen(!isOpen);
     }
+
+    useEffect(() => {
+        if (isOpen) {
+            callbacks.current.onExpand?.();
+        } else {
+            callbacks.current.onCollapse?.();
+        }
+    }, [isOpen]);
 
     return (
         <div className={styles.Accordion}>
