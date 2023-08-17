d152d4e179d3099fb3bd2c4996379eb3cb78ed08
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-08-17 13:32:58 +0300

    [DEV-11282] Inherit prev block alignment for new buttons

--- packages/slate-editor/src/extensions/button-block/lib/insertButtonBlock.ts
+++ packages/slate-editor/src/extensions/button-block/lib/insertButtonBlock.ts
@@ -1,5 +1,8 @@
 import { EditorCommands } from '@prezly/slate-commons';
+import type { Alignment } from '@prezly/slate-types';
+import { isAlignableElement, isImageNode } from '@prezly/slate-types';
 import type { Editor } from 'slate';
+import { Node, Path, Range } from 'slate';
 
 import type { ButtonBlockNode } from '../ButtonBlockNode';
 
@@ -8,10 +11,31 @@ import { createButtonBlock } from './createButtonBlock';
 export function insertButtonBlock(
     editor: Editor,
     props: Partial<Omit<ButtonBlockNode, 'type' | 'children'>> = {},
+    defaultAlignment?: Alignment,
 ) {
-    const button = createButtonBlock(props);
+    const { layout = inferPrevBlockAlignment(editor) ?? defaultAlignment } = props;
+
+    const button = createButtonBlock({ ...props, layout });
 
     EditorCommands.insertNodes(editor, [button], { ensureEmptyParagraphAfter: true });
 
     return button;
+}
+
+function inferPrevBlockAlignment(editor: Editor): Alignment | undefined {
+    const block = prevBlock(editor);
+    if (block && (isAlignableElement(block) || isImageNode(block))) {
+        return block.align;
+    }
+    return undefined;
+}
+
+function prevBlock(editor: Editor): Node | undefined {
+    if (editor.selection && Range.isCollapsed(editor.selection)) {
+        const topLevelPath = editor.selection.focus.path.slice(0, 1);
+        if (Path.hasPrevious(topLevelPath)) {
+            return Node.get(editor, Path.previous(topLevelPath));
+        }
+    }
+    return undefined;
 }
