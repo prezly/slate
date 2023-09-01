76139340f1dae0d1e9017811453bee5545d8fa29
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2023-09-01 16:59:17 +0330

    [DEV-11548] Address incorrect bookmark to video/embed interactions

--- packages/slate-editor/src/extensions/web-bookmark/transforms/convertWebBookmark.ts
+++ packages/slate-editor/src/extensions/web-bookmark/transforms/convertWebBookmark.ts
@@ -1,9 +1,10 @@
-import type { BookmarkNode } from '@prezly/slate-types';
+import type { BookmarkNode, VideoNode } from '@prezly/slate-types';
 import type { Editor } from 'slate';
 import { Transforms } from 'slate';
 
 import type { EmbedNode } from '#extensions/embed';
 import { createEmbed } from '#extensions/embed';
+import { createVideoBookmark } from '#extensions/video';
 
 import type { Presentation } from '../types';
 
@@ -13,6 +14,18 @@ export function convertWebBookmark(
     presentation: Presentation,
 ) {
     if (presentation === 'embed') {
+        if (element.oembed.type === 'video') {
+            Transforms.setNodes<VideoNode>(
+                editor,
+                createVideoBookmark({ oembed: element.oembed, url: element.url }),
+                {
+                    at: [],
+                    match: (node) => node === element,
+                },
+            );
+            return;
+        }
+
         Transforms.setNodes<EmbedNode>(
             editor,
             createEmbed({ oembed: element.oembed, url: element.url }),
