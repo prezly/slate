931f99d4192bc9dad2c124005bd34eac4d580060
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-09-08 12:23:19 +0300

    [CARE-11547] Drop unnecessarily clever interrupted loop
    And replace it with a simple(r) `if ()`.
    
    Thanks @oleg-semyonov
    
    See https://github.com/prezly/slate/pull/498#discussion_r1319569359

--- packages/slate-commons/src/commands/replaceNode.tsx
+++ packages/slate-commons/src/commands/replaceNode.tsx
@@ -17,7 +17,11 @@ export function replaceNode<Original extends Node, New extends Node>(
 ) {
     const { at, match, select = false } = options;
     Editor.withoutNormalizing(editor, () => {
-        for (const [node, path] of Editor.nodes(editor, { at, match, mode: 'highest' })) {
+        const [entry] = Editor.nodes(editor, { at, match, mode: 'highest' });
+
+        if (entry) {
+            const [node, path] = entry;
+
             Transforms.unsetNodes<Original>(editor, Object.keys(node), { at: path });
             Transforms.setNodes<Original | New>(editor, newNode, { at: path });
             if (isElementNode(newNode)) {
@@ -26,10 +30,6 @@ export function replaceNode<Original extends Node, New extends Node>(
             if (select) {
                 Transforms.select(editor, path);
             }
-            // exit after the fist iteration, otherwise there's risk that we'll plant
-            // multiple instances of the same object into the document. Which is bad:
-            // Slate is relying on referential equality and uniqueness internally.
-            return;
         }
     });
 }
