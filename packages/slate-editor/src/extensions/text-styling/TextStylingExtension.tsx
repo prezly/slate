f5a05fb6df205b935a89f837a6da662d5fb80ba6
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-09-14 16:43:17 +0300

    [CARE-1802] Optimize TextStylingExtension by ensuring referential equality

--- packages/slate-editor/src/extensions/text-styling/TextStylingExtension.tsx
+++ packages/slate-editor/src/extensions/text-styling/TextStylingExtension.tsx
@@ -1,5 +1,7 @@
+import type { DeserializeHtml } from '@prezly/slate-commons';
 import { useRegisterExtension } from '@prezly/slate-commons';
 import React from 'react';
+import type { RenderLeafProps } from 'slate-react';
 
 import { Text } from './components';
 import { detectMarks } from './lib';
@@ -7,13 +9,19 @@ import { onKeyDown } from './onKeyDown';
 
 export const EXTENSION_ID = 'TextStylingExtension';
 
+const DESERIALIZE: DeserializeHtml = {
+    marks: detectMarks,
+};
+
 export function TextStylingExtension() {
     return useRegisterExtension({
         id: EXTENSION_ID,
-        deserialize: {
-            marks: detectMarks,
-        },
+        deserialize: DESERIALIZE,
         onKeyDown,
-        renderLeaf: (props) => <Text {...props} />,
+        renderLeaf,
     });
+}
+
+function renderLeaf(props: RenderLeafProps) {
+    return <Text {...props} />;
 }
