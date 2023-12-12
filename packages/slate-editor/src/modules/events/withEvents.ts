6680ad0c47211ae3844c7b5251eabadf8bb1a6c5
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-12-12 17:51:27 +0200

    [DEV-12149] Add `undo` and `redo` tracking events

--- packages/slate-editor/src/modules/events/withEvents.ts
+++ packages/slate-editor/src/modules/events/withEvents.ts
@@ -6,6 +6,19 @@ import type { EditorEventMap } from './types';
 
 export function withEvents(events: Events<EditorEventMap>) {
     return <T extends Editor>(editor: T): T => {
+        const parent = {
+            undo: editor.undo,
+            redo: editor.redo,
+        };
+        editor.undo = () => {
+            events.dispatchEvent('undo');
+            return parent.undo();
+        };
+        editor.redo = () => {
+            events.dispatchEvent('redo');
+            return parent.redo();
+        };
+
         return Object.defineProperty(editor, EVENTS_PROPERTY, { value: events });
     };
 }
