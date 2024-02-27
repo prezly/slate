5374541782fbe6436423e41e4cfb70162a186918
kudlajz <kudlajzzz@gmail.com>
2024-02-27 13:29:00 +0100

    Remove extra props

--- packages/slate-editor/src/extensions/gallery-bookmark/lib/createGalleryBookmark.ts
+++ packages/slate-editor/src/extensions/gallery-bookmark/lib/createGalleryBookmark.ts
@@ -2,25 +2,15 @@ import { BookmarkNode } from '@prezly/slate-types';
 import { v4 as uuidV4 } from 'uuid';
 
 type RequiredProps = Pick<BookmarkNode, 'url' | 'oembed'>;
-type OptionalProps = Omit<BookmarkNode, 'type' | 'children'>;
 
-function withoutExtraAttributes<T extends BookmarkNode>(node: T): BookmarkNode {
-    const { type, children, layout, new_tab, show_thumbnail, uuid, url, oembed, ...extra } = node;
-    if (Object.keys(extra).length === 0) {
-        return node;
-    }
-
-    return { type, children, layout, new_tab, show_thumbnail, uuid, url, oembed };
-}
-
-export function createGalleryBookmark(props: RequiredProps & Partial<OptionalProps>): BookmarkNode {
-    return withoutExtraAttributes({
+export function createGalleryBookmark(props: RequiredProps): BookmarkNode {
+    return {
+        ...props,
         layout: BookmarkNode.Layout.VERTICAL,
-        new_tab: false,
+        new_tab: true,
         show_thumbnail: true,
         uuid: uuidV4(),
-        ...props,
         children: [{ text: '' }],
         type: BookmarkNode.TYPE,
-    });
+    };
 }
