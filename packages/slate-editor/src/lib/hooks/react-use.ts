17c2756932f9475e05d71bebcc6a5f83e1d5788a
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-06-13 17:25:00 +0200

    Fix TS errors when using the package linked to the main app

--- packages/slate-editor/src/lib/hooks/react-use.ts
+++ packages/slate-editor/src/lib/hooks/react-use.ts
@@ -7,6 +7,7 @@
  *
  * This adapter re-exports individual hooks as ES-friendly named exports.
  */
+import type * as T from 'react-use';
 import _useAsyncFn from 'react-use/lib/useAsyncFn.js';
 import _useDebounce from 'react-use/lib/useDebounce.js';
 import _useEffectOnce from 'react-use/lib/useEffectOnce.js';
@@ -30,16 +31,16 @@ function unwrap<T>(module: T | { __esModule: boolean; default: T }): T {
     return module;
 }
 
-export const useAsyncFn = unwrap(_useAsyncFn);
-export const useDebounce = unwrap(_useDebounce);
-export const useEffectOnce = unwrap(_useEffectOnce);
-export const useGetSet = unwrap(_useGetSet);
-export const useLatest = unwrap(_useLatest);
-export const useMount = unwrap(_useMount);
-export const useMountedState = unwrap(_useMountedState);
-export const usePrevious = unwrap(_usePrevious);
-export const useRafLoop = unwrap(_useRafLoop);
-export const useSize = unwrap(_useSize);
-export const useUnmount = unwrap(_useUnmount);
-export const useUpdate = unwrap(_useUpdate);
-export const useUpdateEffect = unwrap(_useUpdateEffect);
+export const useAsyncFn: typeof T.useAsyncFn = unwrap(_useAsyncFn);
+export const useDebounce: typeof T.useDebounce = unwrap(_useDebounce);
+export const useEffectOnce: typeof T.useEffectOnce = unwrap(_useEffectOnce);
+export const useGetSet: typeof T.useGetSet = unwrap(_useGetSet);
+export const useLatest: typeof T.useLatest = unwrap(_useLatest);
+export const useMount: typeof T.useMount = unwrap(_useMount);
+export const useMountedState: typeof T.useMountedState = unwrap(_useMountedState);
+export const usePrevious: typeof T.usePrevious = unwrap(_usePrevious);
+export const useRafLoop: typeof T.useRafLoop = unwrap(_useRafLoop);
+export const useSize: typeof T.useSize = unwrap(_useSize);
+export const useUnmount: typeof T.useUnmount = unwrap(_useUnmount);
+export const useUpdate: typeof T.useUpdate = unwrap(_useUpdate);
+export const useUpdateEffect: typeof T.useUpdateEffect = unwrap(_useUpdateEffect);
