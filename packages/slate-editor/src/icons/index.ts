66e0c8ed0b70925ff1bd410cf488a5a7d984586b
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-06-17 15:20:11 +0200

    [DEV-13013] Replace the custom icons with Lucide icons pack

--- packages/slate-editor/src/icons/index.ts
+++ packages/slate-editor/src/icons/index.ts
@@ -1,7 +1,17 @@
+export {
+    AlignCenter,
+    AlignLeft,
+    AlignRight,
+    Link,
+    Bold as FormatBold,
+    Italic as FormatItalic,
+    Underline as FormatUnderline,
+    Type as FormatStyleNormal,
+    Subscript as FormatStyleSubscript,
+    Superscript as FormatStyleSuperscript,
+} from './lucide';
+
 export { default as Add } from './Add.svg';
-export { default as AlignCenter } from './Align-Center.svg';
-export { default as AlignLeft } from './Align-Left.svg';
-export { default as AlignRight } from './Align-Right.svg';
 export { default as Attachment } from './Attachment.svg';
 export { default as BatsIllustration } from './Bats-Illustration.svg';
 export { default as ButtonLayoutLeft } from './ButtonLayoutLeft.svg';
@@ -64,12 +74,6 @@ export { default as Edit } from './Edit.svg';
 export { default as EmptyNewsroom } from './EmptyNewsroomIcon.svg';
 export { default as Envelope } from './Envelope.svg';
 export { default as ExternalLink } from './ExternalLink.svg';
-export { default as FormatBold } from './Format-Bold.svg';
-export { default as FormatItalic } from './Format-Italic.svg';
-export { default as FormatUnderline } from './Format-Underline.svg';
-export { default as FormatStyleNormal } from './Format-Style-Normal.svg';
-export { default as FormatStyleSubscript } from './Format-Style-Sub.svg';
-export { default as FormatStyleSuperscript } from './Format-Style-Sup.svg';
 export { default as Gallery } from './Gallery.svg';
 export { default as Globe } from './Globe.svg';
 export { default as Image } from './Image.svg';
@@ -85,7 +89,6 @@ export { default as ImageSpacingWide } from './ImageSpacingWide.svg';
 export { default as Info } from './Info.svg';
 export { default as ItemsLayoutHorizontal } from './Items-Layout-Horizontal.svg';
 export { default as ItemsLayoutVertical } from './Items-Layout-Vertical.svg';
-export { default as Link } from './Link.svg';
 export { default as Mobile } from './Mobile.svg';
 export { default as Phone } from './Phone.svg';
 export { default as PlaceholderAttachment } from './Placeholder-Attachment.svg';
