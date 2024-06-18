51491f1dbbd306d7deeaa54098e6623967fcf5f8
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-06-18 14:49:51 +0200

    [DEV-13004] Fix alignment controls for the Callout blocks

--- packages/slate-types/src/lib/isAlignableElement.ts
+++ packages/slate-types/src/lib/isAlignableElement.ts
@@ -1,8 +1,15 @@
 import type { HeadingNode, ListNode, ParagraphNode, QuoteNode } from '../nodes';
+import { CalloutNode } from '../nodes';
 import { isHeadingNode, isListNode, isParagraphNode, isQuoteNode } from '../nodes';
 
 export type AlignableNode = HeadingNode | ListNode | QuoteNode | ParagraphNode;
 
 export function isAlignableElement(node: any): node is AlignableNode {
-    return isParagraphNode(node) || isHeadingNode(node) || isQuoteNode(node) || isListNode(node);
+    return (
+        isParagraphNode(node) ||
+        isHeadingNode(node) ||
+        isQuoteNode(node) ||
+        isListNode(node) ||
+        CalloutNode.isCalloutNode(node)
+    );
 }
