fc0dfc4873dd5d841c71448f537565173eef1c76
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-06-19 13:41:13 +0200

    [DEV-13004] Refactor the CalloutElement component into smaller parts

--- packages/slate-editor/src/extensions/callout/components/CalloutElement.tsx
+++ packages/slate-editor/src/extensions/callout/components/CalloutElement.tsx
@@ -5,8 +5,7 @@ import { Alignment } from '@prezly/slate-types';
 import classNames from 'classnames';
 import EmojiPicker, { EmojiStyle, SuggestionMode } from 'emoji-picker-react';
 import type { HTMLAttributes, Ref } from 'react';
-import { useState } from 'react';
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useCallback, useState } from 'react';
 import { useRootClose } from 'react-overlays';
 import { Popper } from 'react-popper';
 import { useSelected, useSlateStatic } from 'slate-react';
@@ -27,17 +26,18 @@ export const CalloutElement = forwardRef(
         const editor = useSlateStatic();
         const [isPickerOpen, setPickerOpen] = useState(false);
         const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(null);
-        const [pickerElement, setPickerElement] = useState<HTMLElement | null>(null);
 
-        function togglePicker() {
-            setPickerOpen((prev) => !prev);
-        }
+        const togglePicker = useCallback(() => {
+            setPickerOpen((open) => !open);
+        }, []);
+
+        const closePicker = useCallback(() => {
+            setPickerOpen(false);
+        }, []);
 
         const align = element.align ?? Alignment.LEFT;
         const isEmpty = EditorCommands.isNodeEmpty(editor, element);
         const isSelected = useSelected();
-
-        useRootClose(pickerElement, () => setPickerOpen(false));
 
         return (
             <div {...props} ref={ref} className={classNames(className, styles.CalloutElement)}>
@@ -50,60 +50,22 @@ export const CalloutElement = forwardRef(
                         [styles.alignRight]: align === Alignment.RIGHT,
                     })}
                 >
-                    <button
-                        className={classNames(styles.IconButton, {
-                            [styles.empty]: !element.icon,
-                        })}
-                        contentEditable={false}
+                    <IconButton
+                        icon={element.icon ?? null}
+                        onClick={togglePicker}
                         ref={setReferenceElement}
-                        title={
-                            element.icon
-                                ? 'Change icon'
-                                : 'Add icon! If left empty, the icon will not appear in your story.'
-                        }
-                        onClick={() => togglePicker()}
-                    >
-                        {element.icon}
-                    </button>
+                    />
                     {isPickerOpen && (
-                        <Popper
-                            referenceElement={referenceElement ?? undefined}
+                        <Picker
+                            empty={!element.icon}
+                            onClose={closePicker}
+                            onPick={(icon) => {
+                                updateCallout(editor, element, { icon: icon ?? '' });
+                                closePicker();
+                            }}
+                            referenceElement={referenceElement}
                             placement={selectPlacement(align)}
-                            strategy="absolute"
-                        >
-                            {({ ref, style }) => (
-                                <div
-                                    className={styles.Picker}
-                                    ref={mergeRefs(setPickerElement, ref)}
-                                    style={style}
-                                    contentEditable={false}
-                                >
-                                    <EmojiPicker
-                                        onEmojiClick={({ emoji }) => {
-                                            updateCallout(editor, element, { icon: emoji });
-                                            setPickerOpen(false);
-                                        }}
-                                        previewConfig={{ showPreview: false }}
-                                        skinTonesDisabled={true}
-                                        suggestedEmojisMode={SuggestionMode.RECENT}
-                                        emojiStyle={EmojiStyle.APPLE}
-                                    />
-                                    <label className={styles.NoIconOption}>
-                                        <input
-                                            type="checkbox"
-                                            style={{}}
-                                            onInput={() => {
-                                                updateCallout(editor, element, { icon: '' });
-                                                setPickerOpen(false);
-                                            }}
-                                            disabled={!element.icon}
-                                            checked={!element.icon}
-                                        />
-                                        <span>Don&apos;t show icon</span>
-                                    </label>
-                                </div>
-                            )}
-                        </Popper>
+                        />
                     )}
 
                     <p
@@ -125,6 +87,77 @@ export const CalloutElement = forwardRef(
 );
 
 CalloutElement.displayName = 'CalloutElement';
+
+const IconButton = forwardRef(
+    (props: { icon: string | null; onClick: () => void }, ref: Ref<HTMLButtonElement>) => {
+        return (
+            <button
+                className={classNames(styles.IconButton, {
+                    [styles.empty]: !props.icon,
+                })}
+                contentEditable={false}
+                ref={ref}
+                title={
+                    !props.icon
+                        ? 'Add icon! If left empty, the icon will not appear in your story.'
+                        : 'Change icon'
+                }
+                onClick={props.onClick}
+            >
+                {props.icon}
+            </button>
+        );
+    },
+);
+
+IconButton.displayName = 'IconButton';
+
+function Picker(props: {
+    empty: boolean;
+    referenceElement: HTMLElement | null;
+    onPick: (icon: string | null) => void;
+    onClose: () => void;
+    placement: Placement;
+}) {
+    const [pickerElement, setPickerElement] = useState<HTMLElement | null>(null);
+
+    useRootClose(pickerElement, props.onClose);
+
+    return (
+        <Popper
+            referenceElement={props.referenceElement ?? undefined}
+            placement={props.placement}
+            strategy="absolute"
+        >
+            {({ ref, style }) => (
+                <div
+                    className={styles.Picker}
+                    ref={mergeRefs(setPickerElement, ref)}
+                    style={style}
+                    contentEditable={false}
+                >
+                    <EmojiPicker
+                        onEmojiClick={({ emoji }) => props.onPick(emoji)}
+                        previewConfig={{ showPreview: false }}
+                        skinTonesDisabled={true}
+                        suggestedEmojisMode={SuggestionMode.RECENT}
+                        emojiStyle={EmojiStyle.APPLE}
+                    />
+                    <label className={styles.NoIconOption}>
+                        <input
+                            type="checkbox"
+                            style={{}}
+                            onInput={() => props.onPick(null)}
+                            disabled={props.empty}
+                            checked={props.empty}
+                        />
+                        <span>Don&apos;t show icon</span>
+                    </label>
+                </div>
+            )}
+        </Popper>
+    );
+}
 
 function selectPlacement(align: Alignment): Placement {
     if (align === 'left') {
