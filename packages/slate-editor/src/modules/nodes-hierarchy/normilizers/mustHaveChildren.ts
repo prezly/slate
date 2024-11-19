93d3701b6e5ec79feda210a82f056c3334b4e77f
kudlajz <kudlajzzz@gmail.com>
2024-11-19 16:46:25 +0100

    Suppress code using node.children for now

--- packages/slate-editor/src/modules/nodes-hierarchy/normilizers/mustHaveChildren.ts
+++ packages/slate-editor/src/modules/nodes-hierarchy/normilizers/mustHaveChildren.ts
@@ -2,6 +2,7 @@ import type { HierarchyFixer, HierarchyNormalizer } from '../types';
 
 export function mustHaveChildren(fix: HierarchyFixer): HierarchyNormalizer {
     return (editor, node, path) => {
+        // @ts-expect-error TODO: Fix this
         if ('children' in node && node.children.length === 0) {
             const at = [...path, 0];
             return fix(editor, [node, at]);
--- packages/slate-editor/src/modules/nodes-hierarchy/normilizers/removeWhenNoChildren.ts
+++ packages/slate-editor/src/modules/nodes-hierarchy/normilizers/removeWhenNoChildren.ts
@@ -2,6 +2,7 @@ import type { HierarchyNormalizer } from '../types';
 
 export function removeWhenNoChildren(): HierarchyNormalizer {
     return (editor, node, path) => {
+        // @ts-expect-error TODO: Fix this
         if ('children' in node && node.children.length === 0) {
             editor.removeNodes({ at: path, match: (n) => n === node });
             return true;
