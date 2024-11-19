90d037c48a9fa9a446d98e78c6e81f9e92d4f665
kudlajz <kudlajzzz@gmail.com>
2024-11-19 12:47:41 +0100

    Update generic type

--- packages/slate-editor/src/modules/uploadcare/withUploadcare.ts
+++ packages/slate-editor/src/modules/uploadcare/withUploadcare.ts
@@ -1,10 +1,10 @@
-import type { Editor } from 'slate';
+import type { SlateEditor } from '@udecode/plate-common';
 
 import { UPLOADCARE_PROPERTY } from './constants';
 import type { Uploadcare } from './types';
 
 export function withUploadcare(uploads: Uploadcare) {
-    return <T extends Editor>(editor: T): T => {
+    return <T extends SlateEditor>(editor: T): T => {
         return Object.defineProperty(editor, UPLOADCARE_PROPERTY, { value: uploads });
     };
 }
