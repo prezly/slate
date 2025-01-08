1a6f0331c98ec3adbbef917eafd0f9897b8dc032
kudlajz <kudlajzzz@gmail.com>
2025-01-08 12:23:06 +0100

    Use removeNodes instead of editor.removeNodes

--- packages/slate-editor/src/modules/nodes-hierarchy/normilizers/removeWhenNoChildren.ts
+++ packages/slate-editor/src/modules/nodes-hierarchy/normilizers/removeWhenNoChildren.ts
@@ -1,11 +1,11 @@
-import { isElement } from '@udecode/plate-common';
+import { isElement, removeNodes } from '@udecode/plate-common';
 
 import type { HierarchyNormalizer } from '../types';
 
 export function removeWhenNoChildren(): HierarchyNormalizer {
     return (editor, node, path) => {
         if (isElement(node) && node.children.length === 0) {
-            editor.removeNodes({ at: path, match: (n) => n === node });
+            removeNodes(editor, { at: path, match: (n) => n === node });
             return true;
         }
 
