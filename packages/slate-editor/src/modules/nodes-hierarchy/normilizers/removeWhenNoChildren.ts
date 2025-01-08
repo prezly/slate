1a82a85066165b90fef1dbebb8e15ecc8334277f
kudlajz <kudlajzzz@gmail.com>
2025-01-08 12:17:39 +0100

    Drop isEditor check to not drop the editor node

--- packages/slate-editor/src/modules/nodes-hierarchy/normilizers/removeWhenNoChildren.ts
+++ packages/slate-editor/src/modules/nodes-hierarchy/normilizers/removeWhenNoChildren.ts
@@ -1,10 +1,10 @@
-import { isEditor, isElement } from '@udecode/plate-common';
+import { isElement } from '@udecode/plate-common';
 
 import type { HierarchyNormalizer } from '../types';
 
 export function removeWhenNoChildren(): HierarchyNormalizer {
     return (editor, node, path) => {
-        if ((isEditor(node) || isElement(node)) && node.children.length === 0) {
+        if (isElement(node) && node.children.length === 0) {
             editor.removeNodes({ at: path, match: (n) => n === node });
             return true;
         }
