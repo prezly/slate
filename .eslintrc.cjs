bd6c7f35b83108e331a56af4b264cb32182b9c84
kudlajz <kudlajzzz@gmail.com>
2025-01-15 14:52:42 +0100

    Configure ESLint rule around type imports to prefer inline when fixing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -60,7 +60,7 @@ module.exports = {
             }
         ],
         "@typescript-eslint/ban-ts-comment": "warn",
-        "@typescript-eslint/consistent-type-imports": "error",
+        "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: 'inline-type-imports' }],
         "@typescript-eslint/no-empty-interface": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-namespace": "off",
