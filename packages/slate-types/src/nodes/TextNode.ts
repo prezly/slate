94a3fd8fc4aec75bb8baf42fff246a00bc8f467e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2025-01-15 19:26:08 +0100

    [DEV-19130] Fix `slate-types`

--- packages/slate-types/src/nodes/TextNode.ts
+++ packages/slate-types/src/nodes/TextNode.ts
@@ -1,11 +1,10 @@
-import type { TText } from '@udecode/plate-common';
-import type { Node } from 'slate';
+import type { TText, TNode } from '@udecode/plate';
 
 import type { Stylable } from './interfaces';
 import { isObject } from './validation';
 
 export interface TextNode extends TText, Stylable {}
 
-export function isTextNode(node: Node): node is TextNode {
+export function isTextNode(node: TNode): node is TextNode {
     return isObject(node) && 'text' in node;
 }
